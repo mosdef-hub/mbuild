b5cf57482f8689df0821a9d8c10affc54d17e409
Christoph Klein <christoph.t.klein@me.com>
2013-10-25 11:12:43 -0500

    made plotting slightly nicer; still unbearably slow

--- mbuild/atom.py
+++ mbuild/atom.py
@@ -8,6 +8,7 @@ class Atom(object):
         self.atomType = atomType
         self.pos = pos
         self.color = 'k'
+        self.vdw_radius = 1.0
 
     def label(self):
         return self.atomType + '_' + str(id(self))
@@ -22,6 +23,10 @@ class Atom(object):
             return O(pos)
         elif atomType == 'F':
             return F(pos)
+        elif atomType == 'N':
+            return N(pos)
+        elif atomType == 'P':
+            return P(pos)
         else:
             return Atom(atomType, pos)
 
@@ -37,7 +42,10 @@ class Atom(object):
 
     def plot(self, ax, text):
         # print atom
-        ax.scatter(self.pos[0], self.pos[1], self.pos[2], c=self.color, marker='o')
+        ax.scatter(self.pos[0], self.pos[1], self.pos[2], 
+                c=self.color, 
+                marker='o',
+                s=100 * self.vdw_radius ** 3)
         if text is not None:
             ax.text(self.pos[0], self.pos[1], self.pos[2],  text)
         else:
@@ -47,29 +55,42 @@ class Atom(object):
 class C(Atom):
     def __init__(self, pos=(0, 0, 0)):
         super(C, self).__init__('C', pos)
-        self.color = 'y'
+        self.color = 'teal'
+        self.vdw_radius = 1.7
 
 
 class H(Atom):
     def __init__(self, pos=(0, 0, 0)):
         super(H, self).__init__('H', pos)
-        self.color = 'w'
+        self.color = 'white'
+        self.vdw_radius = 1.2
 
 
 class O(Atom):
     def __init__(self, pos=(0, 0, 0)):
         super(O, self).__init__('O', pos)
-        self.color = 'b'
+        self.color = 'red'
+        self.vdw_radius = 1.52
 
 class F(Atom):
     def __init__(self, pos=(0, 0, 0)):
         super(F, self).__init__('F', pos)
-        self.color = 'g'
+        self.color = 'pink'
+        self.vdw_radius = 1.35
 
 
 class N(Atom):
     def __init__(self, pos=(0, 0, 0)):
         super(N, self).__init__('N', pos)
+        self.color = 'blue'
+        self.vdw_radius = 1.55
+
+
+class P(Atom):
+    def __init__(self, pos=(0, 0, 0)):
+        super(P, self).__init__('P', pos)
+        self.color = 'orange'
+        self.vdw_radius = 1.8
 
 
 class G(Atom):
