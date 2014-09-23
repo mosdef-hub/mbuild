51fee5b775e15162ce18b1b2490a5824943ef2f4
Janos Sallai <janos.sallai@vanderbilt.edu>
2014-09-23 12:02:57 -0500

    updated deepcopy logic of compound

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+import os,sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
+from pip.req import parse_requirements
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs]
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -17,32 +20,47 @@ if sys.argv[-1] == 'publish':
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 setup(
     name='mbuild',
     version='0.3.0',
     description='mbuild is a component based molecule builder tool to assemble large molecular systems from reusable parts for molecular dynamics simulations',
     long_description=readme + '\n\n' + history,
-    author='Janos Sallai',
-    author_email='janos.sallai@vanderbilt.edu',
+    author='Janos Sallai, Christoph Klein',
+    author_email='janos.sallai@vanderbilt.edu, christoph.klein@vanderbilt.edu',
     url='https://github.com/sallai/mbuild',
     packages=[
         'mbuild',
     ],
     package_dir={'mbuild': 'mbuild'},
     include_package_data=True,
-    install_requires=[
-    ],
-    license="BSD",
+    install_requires=reqs,
+    license="LGPL",
     zip_safe=False,
     keywords='mbuild',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Beta',
+        'Intended Audience :: Computational Chemistry',
+        'License :: OSI Approved :: LGPL License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
     test_suite='tests',
+    cmdclass={
+      'test':PyTest,
+    },
+    extras_require={
+      'testing': ['pytest'],
+    },
 )
