5a71d2483c3e62cddd6609355fc4ab9e6acaf42f
Janos Sallai <janos.sallai@vanderbilt.edu>
2014-10-13 23:57:05 -0500

    sdist fixes

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ copyright = u'2014, Author'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.0'
+version = '0.4.1'
 # The full version, including alpha/beta/rc tags.
 release = 'beta'
 
