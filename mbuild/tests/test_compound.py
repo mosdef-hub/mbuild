4105941abd0416abae2e855e729f22ef100c189c
Christoph Klein <christoph.t.klein@me.com>
2014-10-23 09:45:33 -0500

    new unit tests

--- mbuild/tests/test_compound.py
+++ mbuild/tests/test_compound.py
@@ -39,3 +39,19 @@ class TestCompound:
 
         only_G = ethane.atom_list_by_kind('G')
         assert sum([1 for x in only_G if x.kind == 'G']) == 16
+
+    def test_bond_list_by_kind(self, ethane):
+        C_H_bonds = ethane.bond_list_by_kind(kind='C-H')
+        assert sum([1 for x in C_H_bonds if x.kind == 'C-H']) == 6
+
+        all_bonds = ethane.bond_list_by_kind()
+        assert len(all_bonds) == 7
+
+    def test_atoms_in_range(self, ethane):
+        group = ethane.atoms_in_range(ethane.atoms[0].pos, 0.141)
+        assert sum([1 for x in group if x.kind == 'H']) == 3
+        assert sum([1 for x in group if x.kind == 'C']) == 2
+
+        group = ethane.atoms_in_range(ethane.atoms[0].pos, 0.141, max_items=4)
+        assert sum([1 for x in group if x.kind == 'H']) == 3
+        assert sum([1 for x in group if x.kind == 'C']) == 1
