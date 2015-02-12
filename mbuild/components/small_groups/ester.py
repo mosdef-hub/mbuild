4197194926cd145a39fc00db9de88a1314d0acc6
Christoph Klein <christoph.t.klein@me.com>
2015-02-12 12:27:47 -0600

    fixed ester

--- mbuild/components/small_groups/ester.py
+++ mbuild/components/small_groups/ester.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from mbuild.compound import Compound
 from mbuild.port import Port
-from mbuild.coordinate_transform import translate
+from mbuild.coordinate_transform import translate, translate_to, rotate_around_z
 
 
 class Ester(Compound):
@@ -14,10 +14,12 @@ class Ester(Compound):
         translate(self, -self.C[0])
 
         self.add(Port(anchor=self.O[1]), 'up')
-        translate(self.up, self.O[1] + np.array([0, 0.07, 0]))
+        rotate_around_z(self.up, np.pi / 2)
+        translate_to(self.up, self.O[1] + np.array([0.07, 0, 0]))
 
         self.add(Port(anchor=self.C[0]), 'down')
-        translate(self.down, np.array([0, -0.07, 0]))
+        rotate_around_z(self.down, np.pi / 2)
+        translate(self.down, np.array([-0.07, 0, 0]))
 
 if __name__ == '__main__':
     m = Ester()
