c832ef56529b047f0df997b1608f96fcf721e34c
Christoph Klein <christoph.t.klein@me.com>
2015-03-25 12:54:03 -0500

    Update sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,17 +24,15 @@ for mod_name in MOCK_MODULES:
 #pip.main(['install', 'sphinxcontrib-napoleon'])
 pip.main(['install', 'numpydoc'])
 
-import mbuild
-import mbuild.version
-
-
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../mbuild'))
+
+import mbuild
+import mbuild.version
 
 # -- General configuration ------------------------------------------------
 
