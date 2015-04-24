c83e6518a838eaaf55e9546c8f68700d3fa04a56
Christoph Klein <christoph.t.klein@me.com>
2015-04-24 09:10:03 -0500

    test fix

--- mbuild/tests/test_atom.py
+++ mbuild/tests/test_atom.py
@@ -4,8 +4,8 @@ from mbuild.tests.base_test import BaseTest
 class TestAtom(BaseTest):
 
     def test_neighbors(self, ethane):
-        neighbors = ethane.C[0].neighbors
+        neighbors = ethane.atoms[0].neighbors
         assert len(neighbors) == 4
-        neighbor_names = (neighbor.name for neighbor in neighbors)
+        neighbor_names = [neighbor.name for neighbor in neighbors]
         assert neighbor_names.count('H') == 3
         assert neighbor_names.count('C') == 1
