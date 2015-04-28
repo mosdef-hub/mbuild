8191e63a9c4cba5094605ade2757586d1cd7d8d0
Christoph Klein <christoph.t.klein@me.com>
2015-04-28 16:46:56 -0500

    replicate in 2 dimensions for test_tiled_compound

--- mbuild/tests/test_tiled_compound.py
+++ mbuild/tests/test_tiled_compound.py
@@ -8,11 +8,11 @@ class TestTiledCompound(BaseTest):
 
     def test_2d_replication(self, betacristobalite):
         nx = 2
-        ny = 1
+        ny = 3
         nz = 1
         tiled = mb.TiledCompound(betacristobalite, [nx, ny, nz])
-        assert tiled.n_atoms == 1800 * nx * ny * nz
-        assert tiled.n_bonds == 2300 * nx * ny * nz
+        assert tiled.n_atoms == 1800 * nx * ny
+        assert tiled.n_bonds == 2300 * nx * ny
 
     def test_incorrect_periodicity(self, betacristobalite):
         nx = 2
