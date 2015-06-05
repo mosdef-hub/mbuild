d6a946d028696545d724ba762a930e7b4acf806c
Trevor Jones <trevor.j.jones@vanderbilt.edu>
2015-06-05 14:05:52 -0500

    removed unwanted changes to alkane_monolayer.

--- mbuild/examples/alkane_monolayer/alkane_monolayer.py
+++ mbuild/examples/alkane_monolayer/alkane_monolayer.py
@@ -39,10 +39,8 @@ def main():
     from mbuild.mask import grid_mask_2d
     mask = grid_mask_2d(8, 8)  # Evenly spaced, 2D grid of points.
     monolayer = AlkaneMonolayer(chain_length=10, mask=mask)
-    monolayer.visualize()
-    monolayer.view_hierarchy()
 
-    # monolayer.save(filename='c10-n64.pdb', show_ports=True)
+    monolayer.save(filename='c10-n64.pdb', show_ports=True)
 
 if __name__ == "__main__":
     main()
