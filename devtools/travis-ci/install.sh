1f19c31937e5ab6c7e5abc604835c72cdfd36961
Christoph Klein <christoph.t.klein@me.com>
2015-07-22 11:31:11 -0500

    Begin cleanup on doc building

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -12,15 +12,3 @@ conda install --yes conda-build jinja2 binstar pip
 conda config --add channels http://conda.anaconda.org/omnia
 conda install --yes mdtraj
 
-# rst -> ipynb -> html conversion
-# pandoc (rst -> md)
-conda install --yes --channel https://conda.anaconda.org/jsw-fnal pandoc
-# notedown (md -> ipynb)
-conda install --yes --channel https://conda.anaconda.org/auto notedown
-# jsonschema: nbconvert and ipython requirements
-conda install --yes jsonschema mistune
-
-# libgmp hack
-if [ ! -f /usr/lib/libgmp.so.3 ]; then
-	sudo ln -s $(dpkg -L libgmp-dev | grep libgmp.so) /usr/lib/libgmp.so.3
-fi
