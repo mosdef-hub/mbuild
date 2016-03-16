45dd45b16597af82cd082e8b190dfaf9ce8d0447
Andrew Summers <andrew.z.summers@gmail.com>
2016-03-16 12:06:39 -0500

    Added test for silica_interface recipe

---
+++ mbuild/tests/test_silica_interface.py
@@ -0,0 +1,28 @@
+import mbuild as mb
+import numpy as np
+from mbuild.tests.base_test import BaseTest
+
+from mbuild.lib.bulk_materials import AmorphousSilica
+
+class TestSilicaInterface(BaseTest):
+
+    def test_silica_interface(self):
+        tile_x = 1
+        tile_y = 1
+        thickness = 1.2
+        oh_density = 5.0
+
+        interface = mb.SilicaInterface(bulk_silica=AmorphousSilica(),
+                                       tile_x=tile_x,
+                                       tile_y=tile_y,
+                                       thickness=thickness,
+                                       oh_density=oh_density)
+
+        thickness_tolerance = 0.3
+        z = [atom.pos[2] for atom in interface.particles() if atom.name in ['Si','O']]
+        assert abs(max(z) - min(z) - thickness) < thickness_tolerance
+
+        density_tolerance = 0.1
+        area = interface.periodicity[0] * interface.periodicity[1]
+        oh_count = len(list(interface.particles_by_name('OS')))
+        assert abs((oh_count/area) - oh_density) < density_tolerance
