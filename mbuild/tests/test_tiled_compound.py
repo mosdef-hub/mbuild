35db394383716746d31a7e1db09cb29945c8c032
Andrew Summers <andrew.z.summers@gmail.com>
2016-03-21 15:37:13 -0500

    Updated tests with correct number of atoms and bonds

--- mbuild/tests/test_tiled_compound.py
+++ mbuild/tests/test_tiled_compound.py
@@ -11,16 +11,16 @@ class TestTiledCompound(BaseTest):
         ny = 3
         nz = 1
         tiled = mb.TiledCompound(betacristobalite, [nx, ny, nz])
-        assert tiled.n_particles == 1800 * nx * ny
-        assert tiled.n_bonds == 2300 * nx * ny
+        assert tiled.n_particles == 1900 * nx * ny
+        assert tiled.n_bonds == 2400 * nx * ny
 
     def test_no_replication(self, betacristobalite):
         nx = 1
         ny = 1
         nz = 1
         tiled = mb.TiledCompound(betacristobalite, [nx, ny, nz])
-        assert tiled.n_particles == 1800 * nx * ny
-        assert tiled.n_bonds == 2300 * nx * ny
+        assert tiled.n_particles == 1900 * nx * ny
+        assert tiled.n_bonds == 2400 * nx * ny
 
     def test_incorrect_periodicity(self, betacristobalite):
         nx = 2
