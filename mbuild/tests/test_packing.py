9f6300bf09ab1f539fa0a2e689a68409597e89b0
Andrew Summers <andrew.z.summers@gmail.com>
2016-06-17 10:34:11 -0500

    added seed to packing functions and additional test

--- mbuild/tests/test_packing.py
+++ mbuild/tests/test_packing.py
@@ -1,5 +1,6 @@
 import pytest
 import sys
+import numpy as np
 
 import mbuild as mb
 from mbuild.tests.base_test import BaseTest
@@ -15,3 +16,10 @@ class TestPacking(BaseTest):
         solvated = mb.solvate(ethane, h2o, n_solvent=100, box=[4, 4, 4])
         assert solvated.n_particles == 8 + 100 * 3
         assert solvated.n_bonds == 7 + 100 * 2
+
+    def test_fill_box_seed(self, ethane):
+        filled = mb.fill_box(ethane, n_compounds=20, box=[2, 2, 2])
+        filled_same = mb.fill_box(ethane, n_compounds=20, box=[2, 2, 2])
+        filled_diff = mb.fill_box(ethane, n_compounds=20, box=[2, 2, 2], seed=2)
+        assert np.array_equal(filled.xyz,filled_same.xyz)
+        assert not np.array_equal(filled.xyz,filled_diff.xyz)
