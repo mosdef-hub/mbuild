e0f1431839e407c21869e00decefe187ee795bc1
Christoph Klein <christoph.t.klein@me.com>
2016-07-20 13:25:55 -0500

    Expand tiled compound testing

--- mbuild/tests/test_tiled_compound.py
+++ mbuild/tests/test_tiled_compound.py
@@ -14,6 +14,12 @@ class TestTiledCompound(BaseTest):
         assert tiled.n_particles == 1900 * nx * ny
         assert tiled.n_bonds == 2400 * nx * ny
 
+        for at in tiled.particles():
+            if at.name.startswith('Si'):
+                assert len(tiled.bond_graph.neighbors(at)) <= 4
+            elif at.name.startswith('O'):
+                assert len(tiled.bond_graph.neighbors(at)) <= 2
+
     def test_no_replication(self, betacristobalite):
         nx = 1
         ny = 1
