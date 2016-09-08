7abd2334903aac258931458c3bbb4cfe751b21c6
Andrew Summers <andrew.z.summers@gmail.com>
2016-09-08 15:10:47 -0500

    Added hoomd writer using new fork (#191)
    * hoomd writer using new fork
    
    * Default mass is not 1 instead of 0
    
    * Test now runs only if foyer installed
    * Bond, angle, dihedral coeffs now printed
    
    * Added description of each section.  Fixed units on coefficients
    * Pair coefficients are now printed

---
+++ mbuild/tests/test_hoomdxml.py
@@ -0,0 +1,21 @@
+import mbuild as mb
+import numpy as np
+import pytest
+from mbuild.tests.base_test import BaseTest
+from mbuild.utils.io import has_foyer
+
+
+class TestHoomdXML(BaseTest):
+
+    @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
+    def test_save(self, ethane):
+        ethane.save(filename='ethane.hoomdxml')
+
+    @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
+    def test_save_forcefield(self, ethane):
+        ethane.save(filename='ethane-opls.hoomdxml',forcefield='opls')
+
+    @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
+    def test_save_box(self, ethane):
+        box = mb.Box(lengths=np.array([2.0,2.0,2.0]))
+        ethane.save(filename='ethane-box.hoomdxml',forcefield='opls',box=box)
