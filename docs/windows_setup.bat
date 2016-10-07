c69fae00dd240a46aa100c2fbcff6d35e802bb27
Janos Sallai <janos.sallai@vanderbilt.edu>
2016-10-07 11:47:30 -0500

    added windows and unix setup scripts

---
+++ docs/windows_setup.bat
@@ -0,0 +1,23 @@
+# create a conda environment called mbuild
+conda create -n mbuild python=3 anaconda
+
+# activate the environmnet
+# windows
+activate mbuild
+# UNIX
+#. activate mbuild
+
+# add conda channels
+conda config --add channels omnia
+conda config --add channels patrickfuller
+
+# install dependencies
+conda install packmol nglview oset parmed mdtraj pytest jupyter nbformat ipykernel imolecule
+
+# clone mbuild
+git clone git@github.com:iModels/mbuild.git
+
+# cd to the mbuild directory
+cd mbuild
+# pip install it with the -e flag
+pip install -e .
