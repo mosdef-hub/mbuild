c3c7a13bb317c3e2d4812aa20bf9a0b9671d81e0
Tengyu Ma <tengyuma@Tengyus-Mac.local>
2016-10-08 17:40:28 -0500

    env setup

--- docs/unix_setup.sh
+++ docs/unix_setup.sh
@@ -1,22 +1,49 @@
 # create a conda environment called mbuild
-conda create -n mbuild python=3 anaconda
+conda create -n iModels python=3 anaconda
 
 # activate the environmnet
 # windows
-# activate mbuild
+# activate iModels
 # UNIX
-. activate mbuild
+. activate iModels
 
 # add conda channels
 conda config --add channels omnia
+conda config --add channels patrickfuller
 
 # install dependencies
-conda install packmol nglview oset parmed mdtraj pytest jupyter nbformat ipykernel
+conda install packmol nglview oset mdtraj pytest jupyter nbformat ipykernel imolecule
 
 # clone mbuild
-git clone git@github.com:iModels/mbuild.git
+echo "****** git clone from source ******"
+git clone https://github.com/iModels/mbuild.git
+git clone https://github.com/iModels/foyer.git
+git clone https://github.com/iModels/metamds.git
+git clone https://github.com/ParmEd/ParmEd.git
+git clone https://github.com/arose/nglview.git
 
+echo "****** install from github source ******"
 # cd to the mbuild directory
 cd mbuild
 # pip install it with the -e flag
+pip install -e .
+
+# install foyer
+cd ..
+cd foyer
+pip install -e .
+
+# install metamds
+cd ..
+cd metamds
+pip install -e .
+
+# install ParmEd
+cd ..
+cd ParmEd
+pip install -e .
+
+# install nglview
+cd ..
+cd nglview
 pip install -e .
