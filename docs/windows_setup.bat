5febdc248cf06bf1bfba9b99372cfb5569913b1e
TengyuMaVandy <tengyuma10717@gmail.com>
2016-10-09 11:20:36 -0500

    init copolymer

--- docs/windows_setup.bat
+++ docs/windows_setup.bat
@@ -1,49 +1,58 @@
-# create a conda environment called mbuild
-conda create -n iModels python=3 anaconda
+@echo off
 
-# activate the environmnet
-# windows
-activate iModels
-# UNIX
-# . activate iModels
+rem create a conda environment called mbuild
+rem conda create -n iModels python=3 anaconda
 
-# add conda channels
+rem activate the environmnet
+rem windows
+rem activate iModels
+rem UNIX
+rem . activate iModels
+
+rem add conda channels
 conda config --add channels omnia
 conda config --add channels patrickfuller
 
-# install dependencies
+rem install dependencies
 conda install packmol nglview oset mdtraj pytest jupyter nbformat ipykernel imolecule
 
-# clone mbuild
-echo "****** git clone from source ******"
+rem clone mbuild
+
+text
+****** git clone from source ******
+endtext
+
 git clone https://github.com/iModels/mbuild.git
 git clone https://github.com/iModels/foyer.git
 git clone https://github.com/iModels/metamds.git
 git clone https://github.com/ParmEd/ParmEd.git
 git clone https://github.com/arose/nglview.git
 
-echo "****** install from github source ******"
-# cd to the mbuild directory
+text
+****** install from github source ******
+endtext
+
+rem cd to the mbuild directory
 cd mbuild
-# pip install it with the -e flag
+rem pip install it with the -e flag
 pip install -e .
 
-# install foyer
+rem install foyer
 cd ..
 cd foyer
 pip install -e .
 
-# install metamds
+rem install metamds
 cd ..
 cd metamds
 pip install -e .
 
-# install ParmEd
+rem install ParmEd
 cd ..
 cd ParmEd
 pip install -e .
 
-# install nglview
+rem install nglview
 cd ..
 cd nglview
 pip install -e .
--- mbuild/recipes/copolymer.py
+++ mbuild/recipes/copolymer.py
@@ -2,38 +2,20 @@ from mbuild.compound import Compound
 from mbuild.coordinate_transform import equivalence_transform
 from mbuild.utils.validation import assert_port_exists
 from mbuild import clone
-from mbuild.recipes import polymer
 
 from mbuild.lib.moieties import CH2, CH3
 from mbuild.lib.moieties import Silane
 
 import itertools
+from numpy.random import permutation
 
 __all__ = ['Copolymer']
 
 
 class Copolymer(Compound):
-    """Connect a component to successive copies of itself.
-
-    Parameters
-    ----------
-    monomers : a set of mb.Compound
-        The set of compounds to replicate by the pattern
-    pattern : the pattern to form a copolymer such as "AABB", "ABAB"
-    port_labels : 2-tuple of strs
-        The names of the two ports to use to connect copies of proto.
-    n : int, optional, default=2
-        The number of times to replicate proto.
-
-    """
+    # Use MainChain() get the main_chain of Copolymer, and then add cap_front and cap_end to it.
 
     def __init__(self, pattern=None, random=False):
-        # parse pattern here
-        # proto_dict = {}
-        # pattern_list = list(pattern)
-        # proto_dict[pattern_list[0]] = monomers[0]
-        # proto_dict[pattern_list[2]] = monomers[1]
-
         """
         :param pattern: General format for all kinds of copolymers.
             [cap_front, [A, A_num, A_port_labels], [B, B_num, B_port_labels], cap_end, repeat_num]
@@ -45,6 +27,48 @@ class Copolymer(Compound):
         """
 
         # default value for pattern
+        super().__init__()
+        if pattern is None:
+            ch2 = CH2()
+            silane = Silane()
+            pattern = [None, [ch2, 2, ("up", "down")], [silane, 2, ("up", "down")], 2, None]
+
+        # get main_chain
+        main_chain = MainChain(pattern, random)
+        self.add(main_chain, 'main_chain')
+
+        # add caps
+        if pattern[0]:
+            self.add(pattern[0], "cap_front")
+            equivalence_transform(self['main_chain'], self['main_chain']['up'], self['cap_front']['up'])
+        else:
+            # Hoist port label to Alkane level.
+            self.add(main_chain['up'], 'up', containment=False)
+
+        if pattern[-1]:
+            self.add(pattern[-1], 'cap_end')
+            equivalence_transform(self['cap_end'], self['cap_end']['up'], self['main_chain']['down'])
+        else:
+            # Hoist port label to Alkane level.
+            self.add(main_chain['down'], 'down', containment=False)
+
+
+class MainChain(Compound):
+    # The main chain for Copolymer. Connect one or more components in a specified sequence.
+
+    def __init__(self, pattern=None, random=False):
+        """
+        :param pattern: General format for all kinds of copolymers.
+            [cap_front, [A, A_num, A_port_labels], [B, B_num, B_port_labels], cap_end, repeat_num]
+            Alternating copolymers: A_num = 1, B_num = 1, repeat_num = any_value
+            Periodic copolymers: A_num = any_value, B_num = any_value, repeat_num = any_value
+            Block copolymers: A_num = any_value, B_num = any_value, repeat_num = 1
+            Statistical copolymers: repeat A block and B block randomly with repeat_num times, if random is True
+        :param random: option for Statistical copolymers, which are actually random copolymers.
+        """
+
+        # default value for pattern
+        super().__init__()
         if pattern is None:
             ch2 = CH2()
             silane = Silane()
@@ -52,71 +76,64 @@ class Copolymer(Compound):
 
         # parse pattern into real chain of copolymer
         copolymer = []
-        port_labels = ("up", "down")  # we need to modify it based on the pattern
-        # 9/28/2016 work here, default port_labels only for test
+        type_A = type(pattern[1][0])
+        type_B = type(pattern[2][0])
+        port_labels_A = pattern[1][2]
+        port_labels_B = pattern[2][2]
+
         for proto in pattern[1:-2]:
-            copolymer.append([proto[0]] * proto[1])  # TypeError: unsupported operand type(s) for *: 'CH2' and 'int'
+            if proto[1] < 1:  # Exception of parse pattern here
+                raise Exception('n must be 1 or more')
+            copolymer.append([proto[0]] * proto[1])  # append multiple times
             for label in proto[2]:
                 assert_port_exists(label, proto[0])  # assert labels in the proto
 
         copolymer = list(itertools.chain.from_iterable(copolymer * pattern[-2]))  # a complete chain for copolymer
-
-        if False:  # Exception of parse pattern here
-            raise Exception('n must be 1 or more')
-        super(Copolymer, self).__init__()
+        if random:
+            copolymer = list(permutation(copolymer))
 
         first_part = None
         last_part = None
         for molecule in copolymer:
             this_part = clone(molecule)
+            # print(isinstance(this_part, type(pattern[2][0])))
             self.add(this_part, 'monomer[$]')
             if last_part is None:
                 first_part = this_part
             else:
                 # Transform this part, such that it's bottom port is rotated
                 # and translated to the last part's top port.
-                equivalence_transform(this_part, this_part.labels[port_labels[1]],
-                                      last_part.labels[port_labels[0]])
+                if isinstance(this_part, type_A):
+                    equivalence_transform(this_part,
+                                          this_part.labels[port_labels_A[1]],
+                                          last_part.labels[port_labels_A[0]])
+                elif isinstance(this_part, type_B):
+                    equivalence_transform(this_part,
+                                          this_part.labels[port_labels_B[1]],
+                                          last_part.labels[port_labels_B[0]])
             last_part = this_part
 
-        # if pattern[0]:  # think about the way in alkane later...
-        #     this_part = pattern[0]
-        #     self.add(this_part, 'monomer[$]')
-        #     equivalence_transform(this_part, this_part.labels[port_labels[1]],
-        #                          last_part.labels[port_labels[0]])
-        # else:
-        #     # Hoist the first part's bottom port to be the bottom port of the copolymer.
-        #     self.add(first_part.labels[port_labels[1]], port_labels[1], containment=False)
-        #
-        # # Hoist the last part's top port to be the top port of the copolymer.
-        # self.add(last_part.labels[port_labels[0]], port_labels[0], containment=False)
+        # Hoist the first part's bottom port to be the bottom port of the polymer.
+        if isinstance(first_part, type_A):
+            self.add(first_part.labels[port_labels_A[1]], port_labels_A[1], containment=False)
+        elif isinstance(first_part, type_B):
+            self.add(first_part.labels[port_labels_B[1]], port_labels_B[1], containment=False)
 
-        # chain = mb.Polymer(CH2(), n=n - 2, port_labels=('up', 'down'))
-        # self.add(chain, 'chain')
-        #
-        # if pattern[0]:
-        #     self.add(CH3(), "methyl_front")
-        #     equivalence_transform(self.monomer, self.monomer.up, self.methyl_front.up)
-        # else:
-        #     # Hoist port label to Alkane level.
-        #     self.add(monomer.up, 'up', containment=False)
-        #
-        # if pattern[-1]:
-        #     self.add(CH3(), 'methyl_end')
-        #     equivalence_transform(self.methyl_end, self.methyl_end.up, self.chain.down)
-        # else:
-        #     # Hoist port label to Alkane level.
-        #     self.add(chain.down, 'down', containment=False)
-
+        # Hoist the last part's top port to be the top port of the polymer.
+        if isinstance(last_part, type_A):
+            self.add(last_part.labels[port_labels_A[0]], port_labels_A[0], containment=False)
+        elif isinstance(last_part, type_B):
+            self.add(last_part.labels[port_labels_B[0]], port_labels_B[0], containment=False)
 
 
 if __name__ == "__main__":
 
-    ch3 = CH3()
-    ch2 = CH2()
-    silane = Silane()
-    # poly = Copolymer(monomers=(ch2, silane), pattern="A3B3", port_labels=("up", "down"))
-    # combine monomers and pattern together
-    pattern = [ch3, [ch2, 2, ("up", "down")], [ch2, 2, ("up", "down")], 2, None]
+    cap_front = CH3()
+    cap_end = CH3()
+
+    chain_A = CH2()
+    chain_B = Silane()
+
+    pattern = [cap_front, [chain_A, 2, ("up", "down")], [chain_B, 2, ("up", "down")], 1, cap_end]
     poly = Copolymer(pattern=pattern, random=False)
-    poly.visualize(show_ports=True)
+    poly.visualize(show_ports=False)
