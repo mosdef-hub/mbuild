828e236752bc8c63ee52020abb22c95e6039f736
Christoph Klein <christoph.t.klein@me.com>
2016-10-11 10:21:22 -0400

    Minor bondgraph optimizations

--- mbuild/bond_graph.py
+++ mbuild/bond_graph.py
@@ -49,53 +49,55 @@ class BondGraph(object):
 
     """
     def __init__(self):
-        self._data = defaultdict(set)
+        self._adj = defaultdict(set)
 
     def add_node(self, node):
         if not self.has_node(node):
-            self._data[node] = set()
+            self._adj[node] = set()
 
     def remove_node(self, node):
+        adj = self._adj
         for other_node in self.nodes():
-            if node in self._data[other_node]:
+            if node in adj[other_node]:
                 self.remove_edge(node, other_node)
 
     def has_node(self, node):
-        return node in self._data
+        return node in self._adj
 
     def nodes(self):
-        return [node for node in self._data]
+        return [node for node in self._adj]
 
     def nodes_iter(self):
-        for node in self._data:
+        for node in self._adj:
             yield node
 
     def number_of_nodes(self):
         return sum(1 for _ in self.nodes_iter())
 
     def add_edge(self, node1, node2):
-        self._data[node1].add(node2)
-        self._data[node2].add(node1)
+        self._adj[node1].add(node2)
+        self._adj[node2].add(node1)
 
     def remove_edge(self, node1, node2):
+        adj = self._adj
         if self.has_node(node1) and self.has_node(node2):
-            self._data[node1].remove(node2)
-            self._data[node2].remove(node1)
-            if not self._data[node1]:
-                del self._data[node1]
-            if not self._data[node2]:
-                del self._data[node2]
+            adj[node1].remove(node2)
+            adj[node2].remove(node1)
+            if not adj[node1]:
+                del adj[node1]
+            if not adj[node2]:
+                del adj[node2]
         else:
             raise ValueError('There is no edge between {} and {}'.format(
                 node1, node2))
 
     def has_edge(self, node1, node2):
         if self.has_node(node1):
-            return node2 in self._data[node1]
+            return node2 in self._adj[node1]
 
     def edges(self):
         edges = set()
-        for node, neighbors in self._data.items():
+        for node, neighbors in self._adj.items():
             for neighbor in neighbors:
                 bond = (node, neighbor) if id(node) < id(neighbor) else (neighbor, node)
                 edges.add(bond)
@@ -110,30 +112,32 @@ class BondGraph(object):
 
     def neighbors(self, node):
         if self.has_node(node):
-            return [neighbor for neighbor in self._data[node]]
+            return [neighbor for neighbor in self._adj[node]]
         else:
             return []
 
     def neighbors_iter(self, node):
         if self.has_node(node):
-            return (neighbor for neighbor in self._data[node])
+            return (neighbor for neighbor in self._adj[node])
         else:
             return iter(())
 
     def compose(self, graph):
-        for node, neighbors in graph._data.items():
+        adj = self._adj
+        for node, neighbors in graph._adj.items():
             if self.has_node(node):
-                self._data[node].union(neighbors)
+                adj[node].union(neighbors)
             elif neighbors:
-                self._data[node] = neighbors
+                adj[node] = neighbors
 
     def subgraph(self, nodes):
         new_graph = BondGraph()
         nodes = list(nodes)
+        adj = self._adj
         for node in nodes:
-            if node not in self._data:
+            if node not in adj:
                 continue
-            for neighbor in self._data[node]:
+            for neighbor in adj[node]:
                 if neighbor in nodes:
                     new_graph.add_edge(node, neighbor)
         return new_graph
