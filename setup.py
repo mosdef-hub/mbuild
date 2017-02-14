87b539d994180b24c14acdc39234555061ed419e
Christoph Klein <christoph.t.klein@me.com>
2017-02-14 17:01:17 -0600

    Tag 0.6.1

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ import subprocess
 from setuptools import setup, find_packages
 
 #####################################
-VERSION = "0.6.0"
-ISRELEASED = False
+VERSION = "0.6.1"
+ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
 else:
