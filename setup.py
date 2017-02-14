a884186297855e91edc98b2fb4e2fec9d6132ca6
Christoph Klein <christoph.t.klein@me.com>
2017-02-14 10:57:31 -0600

    Tag 0.6.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 #####################################
 VERSION = "0.6.0"
-ISRELEASED = False
+ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
 else:
