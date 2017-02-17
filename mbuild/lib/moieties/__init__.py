6c291c95344962ccbbab8bc951e43286ede8e9dd
mattwthompson <mattwthomp@gmail.com>
2017-02-17 12:20:25 -0600

    Add ammonium moiety

--- mbuild/lib/moieties/__init__.py
+++ mbuild/lib/moieties/__init__.py
@@ -3,3 +3,4 @@ from mbuild.lib.moieties.ch3 import CH3
 from mbuild.lib.moieties.ester import Ester
 from mbuild.lib.moieties.h2o import H2O
 from mbuild.lib.moieties.silane import Silane
+from mbuild.lib.moieties.ammonium import Ammonium
