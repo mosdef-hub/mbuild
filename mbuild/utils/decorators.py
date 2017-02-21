4c8fde410896635c78d2fa4ccb571116b19dedac
Tim Moore <tcmoore3@gmail.com>
2017-02-21 07:56:52 -0600

    Clean up deprecated function
    Make warning_string default to empty string to avoid checking its
    contents.

--- mbuild/utils/decorators.py
+++ mbuild/utils/decorators.py
@@ -4,12 +4,10 @@
 from warnings import warn
 
 
-def deprecated(warning_string=None):
+def deprecated(warning_string=''):
     def old_function(fcn):
         def wrapper(*args, **kwargs):
-            printed_message = '{0} is deprecated. '.format(fcn.__name__)
-            if warning_string:
-                printed_message += warning_string
+            printed_message = '{0} is deprecated. {1}'.format(fcn.__name__, warning_string)
             warn(printed_message, DeprecationWarning)
             fcn(*args, **kwargs)
         return wrapper
