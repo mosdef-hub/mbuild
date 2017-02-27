581a8aa34a081dfd9369cbf26215729bed9c7a10
Christoph Klein <christoph.t.klein@me.com>
2017-02-26 21:47:15 -0600

    Fix bug in coarse graining bond creation

--- mbuild/tests/test_pattern.py
+++ mbuild/tests/test_pattern.py
@@ -6,17 +6,17 @@ from mbuild.tests.base_test import BaseTest
 
 class TestPattern(BaseTest):
 
-    def test_apply_to_compound(self, betacristobalite, alkyl, ch3):
+    def test_apply_to_compound(self, betacristobalite, propyl, ch3):
         pattern = mb.Random2DPattern(90)
         chains, backfills = pattern.apply_to_compound(
-            guest=alkyl, host=betacristobalite, backfill=ch3)
+            guest=propyl, host=betacristobalite, backfill=ch3)
         assert len(chains) == 90
         assert len(backfills) == 10
 
         with pytest.raises(AssertionError):
             pattern = mb.Random2DPattern(101)
             chains, backfills = pattern.apply_to_compound(
-                guest=alkyl, host=betacristobalite, backfill=ch3)
+                guest=propyl, host=betacristobalite, backfill=ch3)
 
     def test_random_2d(self):
         pattern = mb.Random2DPattern(100)
