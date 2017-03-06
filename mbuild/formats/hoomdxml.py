27209358490753aea0631790a1cd74c757b19dba
Tim Moore <tcmoore3@gmail.com>
2017-03-06 14:20:49 -0600

    Move RB_to_OPLS out of hoomdxml writer
    It doesn't really make sense to have this in the hoomd writer because we
    use it in the other mBuild writers.

--- mbuild/formats/hoomdxml.py
+++ mbuild/formats/hoomdxml.py
@@ -4,6 +4,8 @@ from math import floor, radians
 
 import numpy as np
 
+from mbuild.utils.conversion import RB_to_OPLS
+
 __all__ = ['write_hoomdxml']
 
 
@@ -305,25 +307,3 @@ def _write_rigid_information(xml_file, rigid_bodies):
         for body in rigid_bodies:
             xml_file.write('{}\n'.format(int(body)))
         xml_file.write('</body>\n')
-
-
-def RB_to_OPLS(c0, c1, c2, c3, c4, c5):
-    """Converts Ryckaert-Bellemans type dihedrals to OPLS type.
-
-    Parameters
-    ----------
-    c0, c1, c2, c3, c4, c5 : Ryckaert-Belleman coefficients (in kcal/mol)
-
-    Returns
-    -------
-    opls_coeffs : np.array, shape=(4,)
-        Array containing the OPLS dihedrals coeffs f1, f2, f3, and f4
-        (in kcal/mol)
-
-    """
-
-    f1 = (-1.5 * c3) - (2 * c1)
-    f2 = c0 + c1 + c3
-    f3 = -0.5 * c3
-    f4 = -0.25 * c4
-    return np.array([f1, f2, f3, f4])
