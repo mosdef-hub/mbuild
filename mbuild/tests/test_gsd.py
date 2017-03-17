d9ea9b7e023a6beb799421cf3deabe640cc5456f
Andrew Summers <andrew.z.summers@gmail.com>
2017-03-17 13:35:44 -0500

    Add test for writing references file
     - Added unit test for the 'references_file' argument to save()
     - Removed the 'has_foyer' dependency from internal writer tests

--- mbuild/tests/test_gsd.py
+++ mbuild/tests/test_gsd.py
@@ -2,7 +2,7 @@ import mbuild as mb
 import numpy as np
 import pytest
 from mbuild.tests.base_test import BaseTest
-from mbuild.utils.io import has_foyer, has_gsd
+from mbuild.utils.io import has_gsd
 
 
 class TestGSD(BaseTest):
@@ -12,12 +12,10 @@ class TestGSD(BaseTest):
         ethane.save(filename='ethane.gsd')
 
     @pytest.mark.skipif(not has_gsd, reason="GSD package not installed")
-    @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
     def test_save_forcefield(self, ethane):
         ethane.save(filename='ethane-opls.gsd', forcefield_name='oplsaa')
 
     @pytest.mark.skipif(not has_gsd, reason="GSD package not installed")
-    @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
     def test_save_box(self, ethane):
         box = mb.Box(lengths=np.array([2.0,2.0,2.0]))
         ethane.save(filename='ethane-box.gsd', forcefield_name='oplsaa',box=box)
