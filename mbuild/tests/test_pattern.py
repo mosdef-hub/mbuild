1fa5cebf31deff7fe41afd2757d5a2a1e610564a
Andrew Summers <andrew.z.summers@gmail.com>
2017-06-08 16:52:27 -0500

    Updated test of scale argument for pattern.apply_to_compound

--- mbuild/tests/test_pattern.py
+++ mbuild/tests/test_pattern.py
@@ -19,13 +19,13 @@ class TestPattern(BaseTest):
                 guest=propyl, host=betacristobalite, backfill=ch3)
 
     def test_apply_to_compound_noscale(self, betacristobalite, propyl):
-        pattern = mb.Random2DPattern(10)
+        pattern = mb.Grid2DPattern(3,3)
         chains, _ = pattern.apply_to_compound(guest=propyl, host=betacristobalite, 
             scale=False)
         chain_positions = [chain.pos for chain in chains]
         for i, pos in enumerate(chain_positions):
             for pos2 in chain_positions[i+1:]:
-                assert betacristobalite.min_periodic_distance(pos, pos2) < 1.75
+                assert betacristobalite.min_periodic_distance(pos, pos2) < 1.5
 
     def test_random_2d(self):
         pattern = mb.Random2DPattern(100)
