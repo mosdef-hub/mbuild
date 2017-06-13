354c1d4e94e1f22ca505c1959843005b55407973
Andrew Summers <andrew.z.summers@gmail.com>
2017-06-13 16:43:26 -0500

    Bump to version 0.7.2, rolling back to MDTraj readers

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ import subprocess
 from setuptools import setup, find_packages
 
 #####################################
-VERSION = "0.7.1"
-ISRELEASED = False
+VERSION = "0.7.2"
+ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
 else:
