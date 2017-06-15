b728eb3c5641b2bca7df4b6eae59314fe38d0571
Logan Guy <thomas.l.guy@vanderbilt.edu>
2017-06-15 16:32:19 -0500

    Cross-tilt enabled
    Cross-tilt was added as another optional parameter, so the user may create
    a cross-tilted bilayer if desired. In addition, the ISIS and HDHD geometries
    were fixed to accomodate new FFA geometry.

--- mbuild/prototypes/hdhd.py
+++ mbuild/prototypes/hdhd.py
@@ -2,26 +2,37 @@ import mbuild as mb
 import numpy as np
 
 from mbuild.prototypes.ffa import FFA
+from mbuild.prototypes.alc import ALC
 
 class HDHD(mb.Compound):
     def __init__(self):
-        super(HDHD,self).__init__()
+        super(HDHD, self).__init__()
         
-        self.add(FFA(17), label='FFA[$]')
-        self.add(FFA(17, hcap=True), label='FFA[$]')
-        self['FFA'][1].remove(self['FFA'][1]['head']['OH'])
-        self['FFA'][1].translate([0,.5,0])
-        self['FFA'][0]['head']['down'].spin(np.pi, 
-                self['FFA'][0]['head']['O'][1].pos-
-                self['FFA'][0]['head']['C'].pos)
-        self['FFA'][0].translate(-self['FFA'][0]['head']['O'][1].pos)
-        self['FFA'][0]['head']['down'].rotate(25*np.pi/180, [0,0,1])
+        self.add(FFA(17), label='acid')
+        self['acid'].translate(-self['acid']['head']['O'][1].pos)
+        self['acid']['head']['down'].rotate(125*np.pi/180, [1,0,0])
         
-        mb.force_overlap(move_this=self['FFA'][1], 
-                         from_positions=self['FFA'][1]['head']['down'], 
-                         to_positions=self['FFA'][0]['head']['down'])
+        self.add(ALC(17), label='alcohol')
+        self['alcohol'].translate(-self['alcohol']['tail'][0].pos)
+        self['alcohol'].add(mb.Port(anchor=self['alcohol']['tail'][0], 
+            orientation=self['alcohol']['head'][0].pos,
+            separation=.15/2), label='up')
+        self['alcohol']['up'].spin(-3*np.pi/4, self['alcohol']['up'].pos)
+        self['alcohol'].remove(self['alcohol']['head'])
+        mb.force_overlap(move_this=self['alcohol'], 
+                from_positions=self['alcohol']['up'],
+                to_positions=self['acid']['head']['down'])
+        
+        self.translate(-self['acid']['tail'][0].pos)
+        self['acid']['tailcap'].rotate(np.pi, self['acid']['head']['C'].pos)
+        
+        self['acid']['tail'].rotate(np.pi, self['acid']['head']['C'].pos)
+        
+        mb.z_axis_transform(self, new_origin = self['acid']['head']['O'][0], 
+                           point_on_z_axis = self['acid']['tailcap'][0])
+        self.rotate(np.pi/2, [0,0,1])
+        self.rotate(np.pi, [0,1,0])
         
 if __name__ == '__main__':
     hdhd = HDHD()
-    hdhd.save('hdhd.mol2')
-
+    hdhd.save('hdhd.mol2', overwrite=True)
--- mbuild/prototypes/isis.py
+++ mbuild/prototypes/isis.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[43]:
-
 import mbuild as mb
 import numpy as np
 
@@ -17,7 +12,7 @@ class ISIS(mb.Compound):
         
         self.add(FFA(17), label='acid')
         self['acid'].translate(-self['acid']['head']['O'][1].pos)
-        self['acid']['head']['down'].rotate(125*np.pi/180, [0,0,1])
+        self['acid']['head']['down'].rotate(125*np.pi/180, [1,0,0])
         self.add(ALC(17), label='alcohol')
         self['alcohol'].translate(-self['alcohol']['tail'][0].pos)
         self['alcohol'].add(mb.Port(anchor=self['alcohol']['tail'][0], 
@@ -38,22 +33,26 @@ class ISIS(mb.Compound):
         mb.force_overlap(move_this=self['alcohol']['methyl'], 
                 from_positions=self['alcohol']['methyl']['up'],
                 to_positions=self['alcohol']['down'])
-    
-        self['acid'].add(mb.Port(anchor=self['acid']['tail'][42], 
-            orientation=(self['acid']['tail'][44].pos -
-                self['acid']['tail'][42].pos),
-            separation=.14/2), label='down')
-        self['acid'].remove(self['acid']['tail'][44])
-        self['acid']['tail'].add(CH3(), label='methyl')
-        mb.force_overlap(move_this=self['acid']['tail']['methyl'], 
-                from_positions=self['acid']['tail']['methyl']['up'],
-                to_positions=self['acid']['down'])
         
         self.translate(-self['acid']['tail'][0].pos)
         self['acid']['tailcap'].rotate(np.pi, self['acid']['head']['C'].pos)
-        
         self['acid']['tail'].rotate(np.pi, self['acid']['head']['C'].pos)
         
+        self.add(mb.Port(anchor=self['acid']['tail'][42], 
+            orientation=(self['acid']['tail'][44].pos -
+                self['acid']['tail'][42].pos),
+            separation=.14/2), label='acid_branch_port')
+        self['acid'].remove(self['acid']['tail'][44])
+        self.add(CH3(), label='acid_branch')
+        mb.force_overlap(move_this=self['acid_branch'], 
+                from_positions=self['acid_branch']['up'],
+                to_positions=self['acid_branch_port'])
+        
+        mb.z_axis_transform(self, new_origin = self['acid']['head']['O'][0], 
+                            point_on_z_axis = self['acid']['tailcap'][0])
+        self.rotate(np.pi/2, [0,0,1])
+        self.rotate(np.pi, [0,1,0])
+
 if __name__ == '__main__':
     isis = ISIS()
     isis.save('isis.mol2', overwrite=True)
