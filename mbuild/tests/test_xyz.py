8f099581930d0f55454751d8fbba05a3685c6144
mattwthompson <matt.thompson@vanderbilt.edu>
2018-03-06 16:49:14 -0600

    Add sanity checks to tests
    This should be cleaned up with an XYZ read that does not require reading in an
    extra file (mdtraj requires a file with topological information, in this case
    PDB file).

--- mbuild/tests/test_xyz.py
+++ mbuild/tests/test_xyz.py
@@ -9,3 +9,7 @@ class TestXYZ(BaseTest):
 
     def test_save(self, ethane):
         ethane.save(filename='ethane.xyz')
+        ethane.save(filename='ethane.pdb')
+        ethane_in = mb.load('ethane.xyz', top='ethane.pdb')
+        assert len(ethane_in.children) == 8
+        assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
