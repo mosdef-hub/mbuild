3f9929642388ad16c37bf34360ef171d8975d766
Christoph Klein <christoph.t.klein@me.com>
2018-03-06 09:23:24 -0800

    Output MD5 mismatches (#409)

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -5,8 +5,11 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x8
 
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+MD5SUM=$(md5sum $MINICONDA | cut -d ' ' -f 1)
+if [[ $MINICONDA_MD5 !=  $MD5SUM ]]; then
     echo "Miniconda MD5 mismatch"
+    echo $MINICONDA_MD5
+    echo $MD5SUM
     exit 1
 fi
 bash $MINICONDA -b
