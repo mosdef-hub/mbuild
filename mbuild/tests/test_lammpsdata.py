6e0ae14a333732720663d7ed92c3a06b9de3bcaa
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2018-03-12 11:04:30 -0500

    Add tests to check for correct 'Atoms' format

--- mbuild/tests/test_lammpsdata.py
+++ mbuild/tests/test_lammpsdata.py
@@ -11,8 +11,49 @@ class TestLammpsData(BaseTest):
         ethane.save(filename='ethane.lammps')
 
     def test_save_forcefield(self, ethane):
-        ethane.save(filename='ethane-opls.lammps', forcefield_name='oplsaa')
+        ethane.save(filename='ethane-opls.lammps',
+                forcefield_name='oplsaa')
 
     def test_save_box(self, ethane):
-        box = mb.Box(lengths=np.array([2.0, 2.0, 2.0]))
-        ethane.save(filename='ethane-box.lammps', forcefield_name='oplsaa', box=box)
+        box = mb.Box(lengths=np.array([2.0, 2.0, 2.0])) ethane.save(filename='ethane-box.lammps',
+        forcefield_name='oplsaa', box=box)
+
+    def test_full_atoms(self, ethane):
+        ethane.save(filename='ethane.lammps', atom_style='full')
+        with open('ethane.lammps', 'r') as f:
+            for line in f:
+                if "Atoms" in line:
+                    for index in range(2):
+                        lines = next(f)
+                    columns = lines.split("\t")
+                    assert len(columns) == 7
+
+    def test_atomic_atoms(self, ethane):
+        ethane.save(filename='ethane.lammps', atom_style='atomic')
+        with open('ethane.lammps', 'r') as f:
+            for line in f:
+                if "Atoms" in line:
+                    for index in range(2):
+                        lines = next(f)
+                    columns = lines.split("\t")
+                    assert len(columns) == 5
+
+    def test_molecular_atoms(self, ethane):
+        ethane.save(filename='ethane.lammps', atom_style='molecular')
+        with open('ethane.lammps', 'r') as f:
+            for line in f:
+                if "Atoms" in line:
+                    for index in range(2):
+                        lines = next(f)
+                    columns = lines.split("\t")
+                    assert len(columns) == 6
+
+    def test_charge_atoms(self, ethane):
+        ethane.save(filename='ethane.lammps', atom_style='charge')
+        with open('ethane.lammps', 'r') as f:
+            for line in f:
+                if "Atoms" in line:
+                    for index in range(2):
+                        lines = next(f)
+                    columns = lines.split("\t")
+                    assert len(columns) == 6
