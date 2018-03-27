ff3cc18e7bf5f367ff69d9ad50cb01b5549d7981
Alex Yang <ahy3nz@virginia.edu>
2018-03-27 17:06:15 -0500

    Use reference atomistic structure to recreate bonds

--- mbuild/__init__.py
+++ mbuild/__init__.py
@@ -1,5 +1,6 @@
 from mbuild.box import Box
 from mbuild.coarse_graining import coarse_grain
+from mbuild.reverse_mapping import reverse_map
 from mbuild.coordinate_transform import *
 from mbuild.compound import *
 from mbuild.pattern import *
