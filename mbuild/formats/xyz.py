567ff504d5ffc109d44af86993677708fd3a38b5
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-23 16:47:16 -0500

    Add simple xyz reader

---
+++ mbuild/formats/xyz.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+import mbuild as mb
+
+__all__ = ['read_xyz']
+
+
+def read_xyz(filename):
+    """Read an XYZ file.
+
+    Parameters
+    ----------
+    filename : str
+        Path of the output file
+
+    Returns
+    -------
+    compound : mb.Compound
+
+    Notes
+    -----
+    The XYZ file format neglects many important details, notably as bonds,
+    residues, and box information.
+
+    """
+
+    compound = mb.Compound()
+
+    with open(filename, 'r') as xyz_file:
+        n_atoms = int(xyz_file.readline())
+        next(xyz_file)
+        coords = np.zeros(shape=(n_atoms, 3), dtype=np.float64)
+        for row, _ in enumerate(coords):
+            line = xyz_file.readline().split()
+            coords[row] = line[1:4]
+            coords[row] *= 0.1
+            particle = mb.Compound(pos = coords[row], name=line[0])
+            compound.add(mb.clone(particle))
+#        coords *= 0.1  # Convert from A to nm
+#
+#    compound = mb.Compound()
+#    for row  in range(n_atoms):
+#        particle = mb.Compound(pos = coords[row])
+#        compound.add(mb.clone(particle))
+
+    return compound
