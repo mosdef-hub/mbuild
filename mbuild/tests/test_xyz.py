a9eff8179a7440f93e8d1c74a93e7b7045fa8fb7
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-23 23:12:06 -0500

    Add io test

---
+++ mbuild/tests/test_xyz.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pytest
+
+import mbuild as mb
+from mbuild.tests.base_test import BaseTest
+
+
+class TestXYZ(BaseTest):
+    def test_load(self, ethane):
+        ethane.save(filename='ethane.xyz')
+        ethane.save(filename='ethane.pdb')
+        ethane_in = mb.load('ethane.xyz', top='ethane.pdb')
+        assert len(ethane_in.children) == 8
+        assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
