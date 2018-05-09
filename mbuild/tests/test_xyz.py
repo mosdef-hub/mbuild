0e29b03015f25571b210cd4f523cbe73d5d89426
mattwthompson <matt.thompson@vanderbilt.edu>
2018-05-09 09:25:58 -0500

    Clean up test

--- mbuild/tests/test_xyz.py
+++ mbuild/tests/test_xyz.py
@@ -8,15 +8,15 @@ from mbuild.tests.base_test import BaseTest
 class TestXYZ(BaseTest):
     def test_load_no_top(self, ethane):
         ethane.save(filename='ethane.xyz')
-        ethane.save(filename='ethane.pdb')
         ethane_in = mb.load('ethane.xyz')
         assert len(ethane_in.children) == 8
+        assert ethane_in.n_bonds == 0
         assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
 
     def test_load_with_top(self, ethane):
         ethane.save(filename='ethane.xyz')
-        ethane.save(filename='ethane.pdb')
-        ethane_in = mb.load('ethane.xyz', top='ethane.pdb')
+        ethane.save(filename='ethane.mol2')
+        ethane_in = mb.load('ethane.xyz', top='ethane.mol2')
         assert len(ethane_in.children) == 8
-        ethane_in.n_bonds == 7
+        assert ethane_in.n_bonds == 7
         assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
