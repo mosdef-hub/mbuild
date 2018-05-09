1f76d70aa1d6a6e2299ac8b8315e8494ea380431
mattwthompson <matt.thompson@vanderbilt.edu>
2018-05-09 08:51:03 -0500

    Rework test with topology

--- mbuild/tests/test_xyz.py
+++ mbuild/tests/test_xyz.py
@@ -5,18 +5,18 @@ import mbuild as mb
 from mbuild.tests.base_test import BaseTest
 
 
-class TestXYZNoTOP(BaseTest):
-    def test_load(self, ethane):
+class TestXYZ(BaseTest):
+    def test_load_no_top(self, ethane):
         ethane.save(filename='ethane.xyz')
         ethane.save(filename='ethane.pdb')
         ethane_in = mb.load('ethane.xyz')
         assert len(ethane_in.children) == 8
         assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
 
-class TestXYZWithTOP(BaseTest):
-    def test_load(self, ethane):
+    def test_load_with_top(self, ethane):
         ethane.save(filename='ethane.xyz')
         ethane.save(filename='ethane.pdb')
         ethane_in = mb.load('ethane.xyz', top='ethane.pdb')
         assert len(ethane_in.children) == 8
+        ethane_in.n_bonds == 7
         assert set([child.name for child in ethane_in.children]) == {'C', 'H'}
