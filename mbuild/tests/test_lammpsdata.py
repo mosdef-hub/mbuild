849ef15714670f2efceb19c906b95244954b20f7
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2018-08-15 16:57:05 -0500

    Test that a structure with nbfix can be written

--- mbuild/tests/test_lammpsdata.py
+++ mbuild/tests/test_lammpsdata.py
@@ -3,6 +3,7 @@ import pytest
 
 import mbuild as mb
 from mbuild.tests.base_test import BaseTest
+from mbuild.formats.lammpsdata import write_lammpsdata
 
 
 class TestLammpsData(BaseTest):
@@ -16,6 +17,16 @@ class TestLammpsData(BaseTest):
     def test_save_box(self, ethane):
         box = mb.Box(lengths=np.array([2.0, 2.0, 2.0]))
         ethane.save(filename='ethane-box.lammps', forcefield_name='oplsaa', box=box)
+
+    def test_nbfix(self, ethane):
+        from foyer import Forcefield
+        OPLSAA = Forcefield(name='oplsaa')
+        structure = OPLSAA.apply(ethane)
+	# Add nbfixes
+        types = list(set([a.atom_type for a in structure.atoms]))
+        types[0].add_nbfix(types[1].name, 1.2, 2.1)
+        types[1].add_nbfix(types[0].name, 1.2, 2.1)
+        write_lammpsdata(filename='/nbfix.lammps', structure=structure)
 
     @pytest.mark.parametrize('atom_style, n_columns', [('full', 7), ('atomic', 5), ('molecular', 6), ('charge', 6)])
     def test_writing_atom_styles(self, ethane, atom_style, n_columns):
