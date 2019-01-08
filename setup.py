8478fca1405320025499e58ce83ae21b7a37562b
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-01-08 17:10:07 -0600

    Bump to version 0.8.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 #####################################
-VERSION = "0.8.1"
+VERSION = "0.8.2"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
