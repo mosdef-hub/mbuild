4c325a0aacc542ec1c619d5f1b69dc71ee92ba86
ahy3nz <alexander.h.yang@vanderbilt.edu>
2019-03-15 15:41:25 -0400

    Undo an init import

--- mbuild/__init__.py
+++ mbuild/__init__.py
@@ -1,6 +1,5 @@
 from mbuild.box import Box
 from mbuild.coarse_graining import coarse_grain
-from mbuild.reverse_mapping import reverse_map
 from mbuild.coordinate_transform import *
 from mbuild.compound import *
 from mbuild.pattern import *
