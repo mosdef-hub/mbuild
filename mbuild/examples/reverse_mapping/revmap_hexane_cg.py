f4b5ebd5fe7988024b3bbb7cdec50631f42c6433
ahy3nz <alexander.h.yang@vanderbilt.edu>
2019-03-15 15:40:45 -0400

    Undo some unnecessary files

--- mbuild/examples/reverse_mapping/revmap_hexane_cg.py
+++
@@ -1,9 +0,0 @@
-import mbuild as mb
-from mapping_moieties.propane_aa import Propane_aa
-from reverse_mapping import *
-
-coarse_grained = mb.load('testing/hexane_cg.mol2')
-mapping_moieties = {'Propane': Propane_aa}
-
-recovered = mb.reverse_map(coarse_grained, mapping_moieties)
-
