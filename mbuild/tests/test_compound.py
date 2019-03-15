129b4b9a7265686c49ea4bb7ab16369197e12bd1
ahy3nz <alexander.h.yang@vanderbilt.edu>
2019-03-15 18:59:06 -0400

    Intentionally raise an exception when testing foyer kwargs

--- mbuild/tests/test_compound.py
+++ mbuild/tests/test_compound.py
@@ -70,13 +70,13 @@ class TestCompound(BaseTest):
 
     @pytest.mark.skipif(not has_foyer, reason="Foyer is not installed")
     def test_save_forcefield_with_file_foyerkwargs(self, methane):
-        exts = ['.gsd', '.hoomdxml', '.lammps', '.lmp', '.top', '.gro',
-                '.mol2', '.pdb', '.xyz']
-        foyerkwargs = {'assert_dihedral_params': False}
-        for ext in exts:
-            methane.save('lythem' + ext,
-                         forcefield_files=get_fn('methane_oplssaa.xml'),
-                         overwrite=True, foyerkwargs=foyerkwargs)
+        exts = ['.hoomdxml']
+        foyerkwargs = {'assert_improper_params': True}
+        with pytest.raises(Exception):
+            for ext in exts:
+                methane.save('lythem' + ext,
+                             forcefield_files=get_fn('methane_oplssaa_broke.xml'),
+                             overwrite=True, foyerkwargs=foyerkwargs)
 
 
     def test_save_resnames(self, ch3, h2o):
