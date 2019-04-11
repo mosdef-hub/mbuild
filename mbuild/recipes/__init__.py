f8f6bd488a5f5f19d3e537aacd4d579f6bae180f
mattwthompson <matt.thompson@vanderbilt.edu>
2019-04-10 20:09:36 -0400

    Rename Recipes to recipes
    There is the possibility that a user may think this is a module, when it is
    actually a class.  But this is a class that simply contains other classes,
    which is more or less what a module is at the level of a user's interaction
    with it.

--- mbuild/recipes/__init__.py
+++ mbuild/recipes/__init__.py
@@ -1,7 +1,7 @@
 class Recipes(object):
     pass
 
-Recipes = Recipes()
+recipes = Recipes()
 from pkg_resources import iter_entry_points
 available_methods = []
 for entry_point in iter_entry_points(group='mbuild.plugins', name=None):
