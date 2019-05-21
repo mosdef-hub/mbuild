9518bc277a83c2bdf3c711c70047807084370a92
Alex Yang <ahy3nz@virginia.edu>
2019-05-20 19:23:08 -0500

    Refactor test protobuf

--- mbuild/tests/test_protobuf.py
+++ mbuild/tests/test_protobuf.py
@@ -1,11 +1,11 @@
 from mbuild.tests.base_test import BaseTest
-from mbuild.formats.protobuff import write_pb3, read_pb3
+from mbuild.formats.protobuf import write_pb2, read_pb2
 
-class TestPB3(BaseTest):
+class TestPB2(BaseTest):
 
     def test_loop(self, ethane):
-        write_pb3(ethane, 'ethane.pb3')
-        proto = read_pb3('ethane.pb3')
+        write_pb2(ethane, 'ethane.pb2')
+        proto = read_pb2('ethane.pb2')
         assert ethane.n_particles == proto.n_particles
         assert ethane.n_bonds == proto.n_bonds
         assert len(ethane.children) == len(proto.children)
