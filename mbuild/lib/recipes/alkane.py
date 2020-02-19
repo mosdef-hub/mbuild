0880796c08c7ee3052d2b69ffa76fb3a590130a3
daico007 <daico007@gmail.com>
2020-02-19 13:30:25 -0600

    refactor

--- mbuild/lib/recipes/alkane.py
+++ mbuild/lib/recipes/alkane.py
@@ -1,7 +1,7 @@
 import mbuild as mb
 
 from mbuild.lib.moieties import CH2
-from mbuild.lib.moieties import CH3
+from mbuild.lib.atoms import H
 
 
 class Alkane(mb.Compound):
@@ -11,33 +11,29 @@ class Alkane(mb.Compound):
 
         Args:
             n: Number of carbon atoms.
-            cap_front: Add methyl group to beginning of chain ('down' port).
-            cap_end: Add methyl group to end of chain ('up' port).
+            cap_front: Add a hydrogen to the beginning of chain ('down' port).
+            cap_end: Add a hydrogen to the end of chain ('up' port).
         """
         if n < 1:
             raise ValueError('n must be 1 or more')
         super(Alkane, self).__init__()
 
         # Adjust length of Polmyer for absence of methyl terminations.
-        if not cap_front:
-            n += 1
-        if not cap_end:
-            n += 1
-        chain = mb.recipes.Polymer(CH2(), n=n-2, port_labels=('up', 'down'))
+        chain = mb.lib.recipes.Polymer(CH2(), n=n, port_labels=('up', 'down'))
         self.add(chain, 'chain')
 
         if cap_front:
-            self.add(CH3(), "methyl_front")
+            self.add(H(), "hydrogen_front")
             mb.force_overlap(move_this=self['chain'],
                              from_positions=self['chain']['up'],
-                             to_positions=self['methyl_front']['up'])
+                             to_positions=self['hydrogen_front']['up'])
         else:
             # Hoist port label to Alkane level.
             self.add(chain['up'], 'up', containment=False)
 
         if cap_end:
-            self.add(CH3(), 'methyl_end')
-            mb.force_overlap(self['methyl_end'], self['methyl_end']['up'], self['chain']['down'])
+            self.add(H(), 'hydrogen_end')
+            mb.force_overlap(self['hydrogen_end'], self['hydrogen_end']['up'], self['chain']['down'])
         else:
             # Hoist port label to Alkane level.
             self.add(chain['down'], 'down', containment=False)
