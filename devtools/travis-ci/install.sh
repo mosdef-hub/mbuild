5e9f04c832ff81828904fb01e42bade5bf522c5f
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-09 19:16:19 -0500

    Follow redirects in MD5 Calculation

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -5,7 +5,7 @@ CONDA_URL=https://repo.anaconda.com/miniconda
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
 
-MINICONDA_MD5=$(curl -s $CONDA_URL | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+MINICONDA_MD5=$(curl -sL $CONDA_URL | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 curl -O $CONDA_URL/$MINICONDA
 
 MD5SUM=$(md5sum $MINICONDA | cut -d ' ' -f 1)
