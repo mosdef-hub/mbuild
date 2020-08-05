f2cd2a8b227340d0fa406621c65604223d5e61ec
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-08-05 12:24:42 -0500

    Use setup.cfg for coverage, bump2version for versioning (Closes #772) (#773)
    * Use setup.cfg for coverage, bump2version for versioning
    
    MBuild didn't follow module level dunder naming conventions for
    its version attribute, which is addressed in this PR. This PR also
    removes .coveragerc and incorporates coverage configuration in setup.cfg.
    
    To test this PR use bump2version (pip install -r bump2version).
             $ bump2version --dry-run patch
    
    * Replace version with __version__ in json_formats.py
    
    * Remove unused import in setup.py
    
    * Fix conf.py in setup.cfg location
    
    * Add bump2version to requirements-dev.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ structures from smaller components.
 
 import os
 import subprocess
-from pathlib import Path
 from setuptools import setup, find_packages
 from distutils.spawn import find_executable
 
@@ -25,66 +24,6 @@ else:
     __version__ = VERSION + '.dev0'
 #####################################
 
-
-def git_version():
-    # Return the git revision as a string
-    # copied from numpy setup.py
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = 'Unknown'
-
-    return GIT_REVISION
-
-
-def write_version_py(version, isreleased, filename):
-    cnt = """
-# This file is generated in setup.py at build time.
-version = '{version}'
-short_version = '{short_version}'
-full_version = '{full_version}'
-git_revision = '{git_revision}'
-release = {release}
-"""
-    base_path = Path(__file__).parent
-    file_path = (base_path / filename)
-    # git_revision
-    if os.path.exists('.git'):
-        git_revision = git_version()
-    else:
-        git_revision = 'Unknown'
-
-    # short_version, full_version
-    if isreleased:
-        full_version = version
-        short_version = version
-    else:
-        full_version = ("{version}+{git_revision}"
-                        .format(version=version, git_revision=git_revision))
-        short_version = version
-
-    with file_path.open('w') as f:
-        f.write(cnt.format(version=version,
-                           short_version=short_version,
-                           full_version=full_version,
-                           git_revision=git_revision,
-                           release=isreleased))
 
 def proto_procedure():
     # Find the Protocol Compiler and compile protocol buffers
@@ -116,8 +55,6 @@ def compile_proto(protoc):
 
 
 if __name__ == '__main__':
-
-    write_version_py(VERSION, ISRELEASED, 'mbuild/version.py')
 
     proto_procedure()
 
