21b4a4a08806f65268dc46d0a3af09f48c8389cc
jennyfothergill <39961845+jennyfothergill@users.noreply.github.com>
2020-11-16 18:28:47 -0700

    update args in test

--- mbuild/tests/test_vasp.py
+++ mbuild/tests/test_vasp.py
@@ -15,45 +15,38 @@ class TestVasp(BaseTest):
         tmpdir.chdir()
 
     def test_write(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=.4123)
+        write_poscar(copper_cell, 'test.poscar')
 
 
     def test_write_direct(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=.4123, coord='direct')
+        write_poscar(copper_cell, 'test.poscar', coord_style='direct')
 
 
     def test_lattice_constant(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=.4123)
+        write_poscar(copper_cell, 'test.poscar', lattice_constant=.4123)
         with open('test.poscar', 'r') as f:
             for i,line in enumerate(f):
                 if i == 1:
-                    lattice_constant = np.genfromtxt(
-                            line.splitlines(True))
+                    lattice_constant = np.genfromtxt(line.splitlines(True))
 
         assert lattice_constant == 0.4123
 
 
     def test_bravais(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=.4123)
+        write_poscar(copper_cell, 'test.poscar')
         with open('test.poscar', 'r') as f:
             bravais = list()
             for i,line in enumerate(f):
                 if i in [2,3,4]:
-                    bravais.append(np.genfromtxt(
-                            line.splitlines(True)))
+                    bravais.append(np.genfromtxt(line.splitlines(True)))
 
-        assert all([a.all() == b.all() for a, b in zip(bravais,
-            [np.array([1,0,0]), np.array([0,1,0]),
-                np.array([0,0,1])])])
+        assert all([
+            a.all() == b.all() for a, b in zip(bravais, [np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])])
+            ])
 
 
     def test_num_elements(self, cscl_crystal):
-        write_poscar(cscl_crystal, 'test.poscar',
-                lattice_constant=.4123)
+        write_poscar(cscl_crystal, 'test.poscar')
         with open('test.poscar', 'r') as f:
             for i,line in enumerate(f):
                 if i == 5:
@@ -63,19 +56,16 @@ class TestVasp(BaseTest):
 
 
     def test_num_atoms(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=0.4123)
+        write_poscar(copper_cell, 'test.poscar')
         with open('test.poscar', 'r') as f:
             for i, line in enumerate(f):
                 pass
-
         assert i + 1 == 44
 
 
     @pytest.mark.parametrize('coord_type', ['direct', 'cartesian'])
     def test_coordinate_header(self, copper_cell, coord_type):
-        write_poscar(copper_cell, 'test.poscar',
-                lattice_constant=0.4123, coord=coord_type)
+        write_poscar(copper_cell, 'test.poscar', coord_style=coord_type)
         with open('test.poscar', 'r') as f:
             for i, line in enumerate(f):
                 if i == 7:
