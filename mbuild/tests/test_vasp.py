ea2ed3500e20246706ebfafa23dbc0fb5a572406
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-11-18 12:28:37 -0700

    blacked

--- mbuild/tests/test_vasp.py
+++ mbuild/tests/test_vasp.py
@@ -5,6 +5,7 @@ import mbuild as mb
 from mbuild.formats.vasp import write_poscar, read_poscar
 from mbuild.tests.base_test import BaseTest
 
+
 class TestVasp(BaseTest):
     """
     Unit tests for Vasp POSCAR writer
@@ -15,84 +16,76 @@ class TestVasp(BaseTest):
         tmpdir.chdir()
 
     def test_read_write(self, gilmerite):
-        write_poscar(gilmerite, 'test.poscar')
-        new_gilmerite = read_poscar('test.poscar')
+        write_poscar(gilmerite, "test.poscar")
+        new_gilmerite = read_poscar("test.poscar")
         assert np.allclose(gilmerite.box.lengths, new_gilmerite.box.lengths)
         assert np.allclose(gilmerite.box.angles, new_gilmerite.box.angles)
         assert np.allclose(gilmerite.xyz, new_gilmerite.xyz)
-
 
     def test_read_write_direct(self, gilmerite):
-        write_poscar(gilmerite, 'test.poscar', coord_style='direct')
-        new_gilmerite = read_poscar('test.poscar')
+        write_poscar(gilmerite, "test.poscar", coord_style="direct")
+        new_gilmerite = read_poscar("test.poscar")
         assert np.allclose(gilmerite.box.lengths, new_gilmerite.box.lengths)
         assert np.allclose(gilmerite.box.angles, new_gilmerite.box.angles)
         assert np.allclose(gilmerite.xyz, new_gilmerite.xyz)
 
-
     def test_lattice_constant(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar', lattice_constant=.4123)
-        with open('test.poscar', 'r') as f:
-            for i,line in enumerate(f):
+        write_poscar(copper_cell, "test.poscar", lattice_constant=0.4123)
+        with open("test.poscar", "r") as f:
+            for i, line in enumerate(f):
                 if i == 1:
                     lattice_constant = np.genfromtxt(line.splitlines(True))
 
         assert lattice_constant == 0.4123
-
 
     def test_bravais(self, copper_cell):
         """
         Test that a compound with no set box (bounding box only)
         has a lattice that is zeros off-diagonal
         """
-        write_poscar(copper_cell, 'test.poscar')
-        with open('test.poscar', 'r') as f:
+        write_poscar(copper_cell, "test.poscar")
+        with open("test.poscar", "r") as f:
             lines = f.readlines()
 
         bravais = np.stack(
-                [np.fromstring(line, sep=" ") for line in lines[2:5]]
-                )
+            [np.fromstring(line, sep=" ") for line in lines[2:5]]
+        )
 
         # zero the diagonal
         for i in range(3):
-            bravais[i,i] = 0
-        assert np.array_equal(bravais, np.zeros((3,3)))
-
+            bravais[i, i] = 0
+        assert np.array_equal(bravais, np.zeros((3, 3)))
 
     def test_num_elements(self, cscl_crystal):
-        write_poscar(cscl_crystal, 'test.poscar')
-        with open('test.poscar', 'r') as f:
-            for i,line in enumerate(f):
+        write_poscar(cscl_crystal, "test.poscar")
+        with open("test.poscar", "r") as f:
+            for i, line in enumerate(f):
                 if i == 5:
                     elements = line.split()
 
         assert len(elements) == 2
 
-
     def test_num_atoms(self, copper_cell):
-        write_poscar(copper_cell, 'test.poscar')
-        with open('test.poscar', 'r') as f:
+        write_poscar(copper_cell, "test.poscar")
+        with open("test.poscar", "r") as f:
             for i, line in enumerate(f):
                 pass
         assert i + 1 == 44
 
-
-    @pytest.mark.parametrize('coord_type', ['direct', 'cartesian'])
+    @pytest.mark.parametrize("coord_type", ["direct", "cartesian"])
     def test_coordinate_header(self, gilmerite, coord_type):
-        write_poscar(gilmerite, 'test.poscar', coord_style=coord_type)
-        with open('test.poscar', 'r') as f:
+        write_poscar(gilmerite, "test.poscar", coord_style=coord_type)
+        with open("test.poscar", "r") as f:
             for i, line in enumerate(f):
                 if i == 7:
-                   coord = line.strip()
+                    coord = line.strip()
 
         assert coord == coord_type
 
-
     def test_warning_raised(self, copper_cell):
         with pytest.warns(UserWarning):
-            write_poscar(copper_cell, 'test.poscar', coord_style="direct")
-
+            write_poscar(copper_cell, "test.poscar", coord_style="direct")
 
     def test_error_raised(self, copper_cell):
         with pytest.raises(ValueError):
-            write_poscar(copper_cell, 'test.poscar', coord_style="heck")
+            write_poscar(copper_cell, "test.poscar", coord_style="heck")
