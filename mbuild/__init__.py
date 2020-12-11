0853841e4197c6129781692b1acc89b581dbf1c7
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-12-11 15:58:08 -0600

    Bump to version 0.10.11

--- mbuild/__init__.py
+++ mbuild/__init__.py
@@ -8,4 +8,4 @@ from mbuild.conversion import load
 from mbuild.port import Port
 from mbuild.lattice import Lattice
 from mbuild.recipes import recipes
-__version__ = '0.10.10'
+__version__ = '0.10.11'
