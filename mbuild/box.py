69fd8013ac3c811af5d5f19512f2860276730bdb
Matt Thompson <matt.thompson@vanderbilt.edu>
2021-02-11 14:54:10 -0600

    Remove deprecated aliases from NumPy to built-in types (#828)

--- mbuild/box.py
+++ mbuild/box.py
@@ -27,8 +27,8 @@ class Box(object):
                     "You provided: "
                     "lengths={} mins={} maxs={}".format(lengths, mins, maxs)
                 )
-            mins = np.array(mins, dtype=np.float)
-            maxs = np.array(maxs, dtype=np.float)
+            mins = np.array(mins, dtype=float)
+            maxs = np.array(maxs, dtype=float)
             assert mins.shape == (3, ), "Given mins have wrong dimensions"
             assert maxs.shape == (3, ), "Given maxs have wrong dimensions"
             assert all(mins <= maxs), "Given mins are greater than maxs"
@@ -43,9 +43,9 @@ class Box(object):
                     "lengths={} mins={} maxs={}".format(lengths, mins, maxs)
                 )
             if isinstance(lengths, int) or isinstance(lengths, float):
-                lengths = np.array(lengths*np.ones(3), dtype=np.float)
+                lengths = np.array(lengths*np.ones(3), dtype=float)
             else:
-                lengths = np.array(lengths, dtype=np.float)
+                lengths = np.array(lengths, dtype=float)
             assert lengths.shape == (3, )
             assert all(lengths >= 0), "Given lengths are negative"
             self._mins = _BoxArray(array=(0,0,0), var="mins", box=self)
@@ -75,7 +75,7 @@ class Box(object):
 
     @mins.setter
     def mins(self, mins):
-        mins = np.array(mins, dtype=np.float)
+        mins = np.array(mins, dtype=float)
         assert mins.shape == (3, )
         assert all(mins <= self.maxs), "Given mins is greater than maxs"
         self._mins = _BoxArray(array=mins, var="mins", box=self)
@@ -83,7 +83,7 @@ class Box(object):
 
     @maxs.setter
     def maxs(self, maxs):
-        maxs = np.array(maxs, dtype=np.float)
+        maxs = np.array(maxs, dtype=float)
         assert maxs.shape == (3, )
         assert all(maxs >= self.mins), "Given maxs is less than mins"
         self._maxs = _BoxArray(array=maxs, var="maxs", box=self)
@@ -92,20 +92,20 @@ class Box(object):
     @lengths.setter
     def lengths(self, lengths):
         if isinstance(lengths, int) or isinstance(lengths, float):
-            lengths = np.array(lengths*np.ones(3), dtype=np.float)
+            lengths = np.array(lengths*np.ones(3), dtype=float)
         else:
-            lengths = np.array(lengths, dtype=np.float)
+            lengths = np.array(lengths, dtype=float)
         assert lengths.shape == (3, )
         assert all(lengths >= 0), "Given lengths are negative" 
         self._maxs = _BoxArray(array=(self.maxs + (0.5*lengths - 0.5*self.lengths)), var="maxs", box=self)
         self._mins = _BoxArray(array=(self.mins - (0.5*lengths - 0.5*self.lengths)), var="mins", box=self)
-        self._lengths = _BoxArray(array=lengths, var="lengths", box=self, dtype=np.float)
+        self._lengths = _BoxArray(array=lengths, var="lengths", box=self, dtype=float)
 
     @angles.setter
     def angles(self, angles):
-        angles = np.array(angles, dtype=np.float)
+        angles = np.array(angles, dtype=float)
         assert angles.shape == (3, )
-        self._angles = _BoxArray(array=angles, var="angles", box=self, dtype=np.float)
+        self._angles = _BoxArray(array=angles, var="angles", box=self, dtype=float)
 
     def __repr__(self):
         return "Box(mins={}, maxs={}, angles={})".format(self.mins, self.maxs, self.angles)
@@ -127,7 +127,7 @@ class _BoxArray(np.ndarray):
     box : mb.Box
         This is the Box contains this attribute (one level up of this array)
     """
-    def __new__(cls, array, var=None, box=None, dtype=np.float):
+    def __new__(cls, array, var=None, box=None, dtype=float):
         _array = np.asarray(array, dtype).view(cls)
         _array.var = var
         _array.box = box
