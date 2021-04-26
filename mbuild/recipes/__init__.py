644ee50cc03b423f54505793821158dacea54b80
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-26 17:02:14 -0600

    add docstrings

--- mbuild/recipes/__init__.py
+++ mbuild/recipes/__init__.py
@@ -1,8 +1,15 @@
+"""Entrypoints for mBuild recipe plugins."""
+
+
 class Recipes(object):
+    """mBuild recipe object."""
+
     pass
+
 
 recipes = Recipes()
 from pkg_resources import iter_entry_points
+
 available_methods = []
-for entry_point in iter_entry_points(group='mbuild.plugins', name=None):
+for entry_point in iter_entry_points(group="mbuild.plugins", name=None):
     setattr(recipes, entry_point.name, entry_point.load())
