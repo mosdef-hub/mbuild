22c7c639e1378376ac5be8e132d199dd82cd3464
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-26 19:38:03 -0600

    pre-commit fixes

--- mbuild/formats/xyz.py
+++ mbuild/formats/xyz.py
@@ -1,18 +1,22 @@
+"""XYZ format."""
+from warnings import warn
+
 import numpy as np
+from ele import element_from_symbol
+from ele.exceptions import ElementError
 
 import mbuild as mb
 from mbuild.exceptions import MBuildError
-from ele import element_from_symbol
-from ele.exceptions import ElementError
-from warnings import warn
 
-__all__ = ['read_xyz', 'write_xyz']
+__all__ = ["read_xyz", "write_xyz"]
 
 
 def read_xyz(filename, compound=None):
-    """Read an XYZ file. The expected format is as follows:
-    The first line contains the number of atoms in the file The second line
-    contains a comment, which is not read.  Remaining lines, one for each
+    """Read an XYZ file.
+
+    The expected format is as follows:
+    The first line contains the number of atoms in the file. The second line
+    contains a comment, which is not read. Remaining lines, one for each
     atom in the file, include an elemental symbol followed by X, Y, and Z
     coordinates in Angstroms. Columns are expected tbe separated by
     whitespace. See https://openbabel.org/wiki/XYZ_(format).
@@ -24,7 +28,7 @@ def read_xyz(filename, compound=None):
 
     Returns
     -------
-    compound : mb.Compound
+    compound : Compound
 
     Notes
     -----
@@ -35,22 +39,23 @@ def read_xyz(filename, compound=None):
     guaranteed to be compatible with this reader. For example, the TINKER
     XYZ format is not expected to be properly read.
     """
-
     if compound is None:
         compound = mb.Compound()
 
     guessed_elements = set()
 
-    with open(filename, 'r') as xyz_file:
+    with open(filename, "r") as xyz_file:
         n_atoms = int(xyz_file.readline())
         xyz_file.readline()
         coords = np.zeros(shape=(n_atoms, 3), dtype=np.float64)
         for row, _ in enumerate(coords):
             line = xyz_file.readline().split()
             if not line:
-                msg = ('Incorrect number of lines in input file. Based on the '
-                       'number in the first line of the file, {} rows of atoms '
-                       'were expected, but at least one fewer was found.')
+                msg = (
+                    "Incorrect number of lines in input file. Based on the "
+                    "number in the first line of the file, {} rows of atoms "
+                    "were expected, but at least one fewer was found."
+                )
                 raise MBuildError(msg.format(n_atoms))
             coords[row] = line[1:4]
             coords[row] *= 0.1
@@ -74,9 +79,11 @@ def read_xyz(filename, compound=None):
         # Verify we have read the last line by ensuring the next line in blank
         line = xyz_file.readline().split()
         if line:
-            msg = ('Incorrect number of lines in input file. Based on the '
-                   'number in the first line of the file, {} rows of atoms '
-                   'were expected, but at least one more was found.')
+            msg = (
+                "Incorrect number of lines in input file. Based on the "
+                "number in the first line of the file, {} rows of atoms "
+                "were expected, but at least one more was found."
+            )
             raise MBuildError(msg.format(n_atoms))
 
     return compound
@@ -99,19 +106,17 @@ def write_xyz(structure, filename):
 
     The XYZ file format neglects many important details, notably as bonds,
     residues, and box information.
-
     """
-
     if isinstance(structure, mb.Compound):
-        raise ValueError(
-            'Expected a ParmEd structure, got an mbuild.Compound'
-        )
+        raise ValueError("Expected a ParmEd structure, got an mbuild.Compound")
 
     xyz = np.array([[atom.xx, atom.xy, atom.xz] for atom in structure.atoms])
     types = [atom.name for atom in structure.atoms]
 
-    with open(filename, 'w') as xyz_file:
+    with open(filename, "w") as xyz_file:
         xyz_file.write(str(len(structure.atoms)))
-        xyz_file.write('\n' + filename+' - created by mBuild\n')
+        xyz_file.write("\n" + filename + " - created by mBuild\n")
         for typ, coords in zip(types, xyz):
-            xyz_file.write('{:s} {:11.6f} {:11.6f} {:11.6f}\n'.format(typ, *coords))
+            xyz_file.write(
+                "{:s} {:11.6f} {:11.6f} {:11.6f}\n".format(typ, *coords)
+            )
