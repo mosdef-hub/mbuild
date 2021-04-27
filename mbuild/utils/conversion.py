62074afda395008e6bf54e4096bac26a7c08c72a
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-27 14:32:23 -0600

    use math formatting for equations, precommit fixes

--- mbuild/utils/conversion.py
+++ mbuild/utils/conversion.py
@@ -23,10 +23,28 @@ def RB_to_OPLS(c0, c1, c2, c3, c4, c5):
 
 
 def RB_to_CHARMM(c0, c1, c2, c3, c4, c5):
-    """Converts Ryckaert-Bellemans (RB) type dihedrals to CHARMM type
-    or
-    RB_torsions = c0 + c1*Cos[Psi] + c2*Cos[Psi]^2 + c3*Cos[Psi]^3 + c4*Cos[Psi]^4 + c5*Cos[5*Psi]^5
-    where Psi= t-Pi = t - 180 degrees
+    r"""Convert Ryckaert-Bellemans (RB) type dihedrals to CHARMM type.
+
+    .. math::
+        RB_torsions &= c0 + c1*cos(psi) + c2*cos(psi)^2 + c3*cos(psi)^3 + \\
+                    &= c4*cos(psi)^4 + c5*cos(5*psi)^5
+
+    where :math:`psi = t - pi = t - 180 degrees`
+
+    .. math::
+        CHARMM_torsions &= K0 * (1 + cos(n0*t - d0)) + \\
+                        &= K1 * (1 + cos(n1*t - d1)) + \\
+                        &= K2 * (1 + cos(n2*t - d2)) + \\
+                        &= K3 * (1 + cos(n3*t - d3)) + \\
+                        &= K4 * (1 + cos(n4*t - d4)) + \\
+                        &= K5 * (1 + cos(n5*t - d5))
+
+        CHARMM_torsions &= K0 +
+                        &= K1 * (1 + cos(n1*t - d1)) + \\
+                        &= K2 * (1 + cos(n2*t - d2)) + \\
+                        &= K3 * (1 + cos(n3*t - d3)) + \\
+                        &= K4 * (1 + cos(n4*t - d4)) + \\
+                        &= K5 * (1 + cos(n5*t - d5))
 
     Parameters
     ----------
@@ -41,27 +59,25 @@ def RB_to_CHARMM(c0, c1, c2, c3, c4, c5):
 
     d0 = 90
     d1 = 180
-    d2 =  0
-    d3 =  180
+    d2 = 0
+    d3 = 180
     d4 = 0
     d5 = 180
 
     Returns
     -------
-    CHARMM_torsions =
-    = K0 * (1 + Cos[n0*(t) - (d0)] ) + K1 * (1 + Cos[n1*(t) - (d1)] ) + K2 * (1 + Cos[n2*(t) - (d2)] )
-    + K3 * (1 + Cos[n3*(t) - (d3)] )  +  K4 * (1 + Cos[n4*(t) - (d4)] )  + K5 * (1 + Cos[n5*(t) - (d5)] )
-
-    = K0 + K1 * (1 + Cos[n1*(t) - (d1)] ) + K2 * (1 + Cos[n2*(t) - (d2)] )
-    + K3 * (1 + Cos[n3*(t) - (d3)] )  +  K4 * (1 + Cos[n4*(t) - (d4)] )  + K5 * (1 + Cos[n5*(t) - (d5)] )
-    K0, K1, K2, K3, K4, K5, n0, n1, n2, n3, n4, n5, d0, d1, d2, d3, d4, and d5  : Charmm coefficients (in kcal/mol)
-
     CHARMM_dihedral coeffs : np.matrix, shape=(6,3)
-        Array containing the CHARMM dihedral coeffs  [[K0, n0, d0], [K1, n1, d1], [K2, n2, d2], [K3, n3, d3],
-        [K4, n4, d4], [K5, n5, d5]]  (in kcal/mol)
-    """
+        Array containing the CHARMM dihedral coeffs (in kcal/mol):
 
-    # see below or the long version is,  K0 = (c0 + c2 / 2 + 3 / 8 * c4) - K1 - K2 - K3 - K4 - K5
+        [[K0, n0, d0],
+         [K1, n1, d1],
+         [K2, n2, d2],
+         [K3, n3, d3],
+         [K4, n4, d4],
+         [K5, n5, d5]]
+    """
+    # see below or the long version is,
+    # K0 = (c0 + c2 / 2 + 3 / 8 * c4) - K1 - K2 - K3 - K4 - K5
     K0 = c0 - c1 - c3 - (c4 / 4) - c5
     K1 = c1 + (3 / 4) * c3 + (5 / 8) * c5
     K2 = (1 / 2) * c2 + (1 / 2) * c4
@@ -96,10 +112,10 @@ def RB_to_CHARMM(c0, c1, c2, c3, c4, c5):
 
 
 def base10_to_base62_alph_num(base10_no):
-    """Converts base-10 integer to base-62 alphanumeric system
+    """Convert base-10 integer to base-62 alphanumeric system.
 
-    This function provides a utility to write pdb/psf files such that it
-    can add may more than 9999 atoms and 999 residues.
+    This function provides a utility to write pdb/psf files such that it can
+    add may more than 9999 atoms and 999 residues.
 
     Parameters
     ----------
@@ -119,10 +135,10 @@ def base10_to_base62_alph_num(base10_no):
 
 
 def base10_to_base52_alph(base10_no):
-    """Converts base-10 integer to base-52 alphabetic system
+    """Convert base-10 integer to base-52 alphabetic system.
 
-    This function provides a utility to write pdb/psf files such that it
-    can add more atom types in the 3 or 4 character limited pdb and psf files
+    This function provides a utility to write pdb/psf files such that it can
+    add more atom types in the 3 or 4 character limited pdb and psf files
 
     Parameters
     ----------
@@ -137,16 +153,15 @@ def base10_to_base52_alph(base10_no):
     See Also
     --------
     mbuild.conversion._to_base: Helper function to perform a base-n conversion
-
     """
     return _to_base(number=base10_no, base=52)
 
 
 def base10_to_base26_alph(base10_no):
-    """Converts base-10 integer to base-26 alphabetic system
+    """Convert base-10 integer to base-26 alphabetic system.
 
-    This function provides a utility to write pdb/psf files such that it
-    can add many more than 9999 atoms and 999 residues.
+    This function provides a utility to write pdb/psf files such that it can
+    add many more than 9999 atoms and 999 residues.
 
     Parameters
     ----------
@@ -166,10 +181,10 @@ def base10_to_base26_alph(base10_no):
 
 
 def base10_to_base16_alph_num(base10_no):
-    """Converts base-10 integer to base-16 hexadecimal system
+    """Convert base-10 integer to base-16 hexadecimal system.
 
-    This function provides a utility to write pdb/psf files such that it
-    can add many more than 9999 atoms and 999 residues.
+    This function provides a utility to write pdb/psf files such that it can
+    add many more than 9999 atoms and 999 residues.
 
     Parameters
     ----------
@@ -190,12 +205,12 @@ def base10_to_base16_alph_num(base10_no):
 
 # Helpers to convert base
 def _to_base(number, base=62):
-    """Convert a base-10 number into base-n alpha-num"""
+    """Convert a base-10 number into base-n alpha-num."""
     start_values = {62: "0", 52: "A", 26: "A"}
     if base not in start_values:
         raise ValueError(
-            f"Base-{base} system is not supported."
-            f"Supported bases are: {list(start_values.keys())}"
+            f"Base-{base} system is not supported. Supported bases are: "
+            f"{list(start_values.keys())}"
         )
 
     num = 1
@@ -226,7 +241,7 @@ def _to_base(number, base=62):
 
 
 def _digit_to_alpha_num(digit, base=52):
-    """Helper function to convert digit to base-n"""
+    """Convert digit to base-n."""
     base_values = {
         26: {j: chr(j + 65) for j in range(0, 26)},
         52: {j: chr(j + 65) if j < 26 else chr(j + 71) for j in range(0, 52)},
@@ -235,8 +250,8 @@ def _digit_to_alpha_num(digit, base=52):
 
     if base not in base_values:
         raise ValueError(
-            f"Base-{base} system is not supported."
-            f"Supported bases are: {list(base_values.keys())}"
+            f"Base-{base} system is not supported. Supported bases are: "
+            f"{list(base_values.keys())}"
         )
 
     return base_values[base].get(digit, digit)
