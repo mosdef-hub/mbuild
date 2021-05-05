5beca2e180bcd42e77a3b03cef1f6a7d05536369
Ryan S. DeFever <r.s.defever@gmail.com>
2021-05-05 16:33:28 -0400

    Simplify unit tests with pytest.mark.parametrize

--- mbuild/tests/test_lib_molecule.py
+++ mbuild/tests/test_lib_molecule.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from mbuild.lib.molecules import (
     WaterSPC,
@@ -11,8 +12,16 @@ from mbuild.tests.base_test import BaseTest
 
 
 class TestWater(BaseTest):
-    def test_tip3p(self):
-        water = WaterTIP3P()
+
+    @pytest.mark.parametrize(
+        "model, bond_length, angle",
+        [
+            (WaterTIP3P, 0.09572, 104.52),
+            (WaterSPC, 0.1, 109.47)
+        ],
+    )
+    def test_water_3site(self, model, bond_length, angle):
+        water = model()
         o1 = [p for p in water.particles_by_name("OW")]
         assert len(o1) == 1
         o1 = o1[0]
@@ -24,43 +33,27 @@ class TestWater(BaseTest):
         h2 = h2[0]
         v1 = h1.xyz[0] - o1.xyz[0]
         v2 = h2.xyz[0] - o1.xyz[0]
-        assert np.allclose(np.linalg.norm(v1), 0.09572)
-        assert np.allclose(np.linalg.norm(v2), 0.09572)
+        assert np.allclose(np.linalg.norm(v1), bond_length)
+        assert np.allclose(np.linalg.norm(v2), bond_length)
         assert np.allclose(
             np.degrees(
                 np.arccos(
                     np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
                 )
             ),
-            104.52,
+            angle,
         )
 
-    def test_spc(self):
-        water = WaterSPC()
-        o1 = [p for p in water.particles_by_name("OW")]
-        assert len(o1) == 1
-        o1 = o1[0]
-        h1 = [p for p in water.particles_by_name("HW1")]
-        assert len(h1) == 1
-        h1 = h1[0]
-        h2 = [p for p in water.particles_by_name("HW2")]
-        assert len(h2) == 1
-        h2 = h2[0]
-        v1 = h1.xyz[0] - o1.xyz[0]
-        v2 = h2.xyz[0] - o1.xyz[0]
-        assert np.allclose(np.linalg.norm(v1), 0.1)
-        assert np.allclose(np.linalg.norm(v2), 0.1)
-        assert np.allclose(
-            np.degrees(
-                np.arccos(
-                    np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-                )
-            ),
-            109.47,
-        )
-
-    def test_tip4p(self):
-        water = WaterTIP4P()
+    @pytest.mark.parametrize(
+        "model, bond_length, vsite_length, angle",
+        [
+            (WaterTIP4P, 0.09572, 0.015, 104.52),
+            (WaterTIP4PIce, 0.09572, 0.01577, 104.52),
+            (WaterTIP4P2005, 0.09572, 0.01546, 104.52)
+        ],
+    )
+    def test_water_4site(self, model, bond_length, vsite_length, angle):
+        water = model()
         o1 = [p for p in water.particles_by_name("OW")]
         assert len(o1) == 1
         o1 = o1[0]
@@ -76,72 +69,14 @@ class TestWater(BaseTest):
         v1 = h1.xyz[0] - o1.xyz[0]
         v2 = h2.xyz[0] - o1.xyz[0]
         v3 = m1.xyz[0] - o1.xyz[0]
-        assert np.allclose(np.linalg.norm(v1), 0.09572)
-        assert np.allclose(np.linalg.norm(v2), 0.09572)
-        assert np.allclose(np.linalg.norm(v3), 0.015)
+        assert np.allclose(np.linalg.norm(v1), bond_length)
+        assert np.allclose(np.linalg.norm(v2), bond_length)
+        assert np.allclose(np.linalg.norm(v3), vsite_length)
         assert np.allclose(
             np.degrees(
                 np.arccos(
                     np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
                 )
             ),
-            104.52,
-        )
-
-    def test_tip4pice(self):
-        water = WaterTIP4PIce()
-        o1 = [p for p in water.particles_by_name("OW")]
-        assert len(o1) == 1
-        o1 = o1[0]
-        h1 = [p for p in water.particles_by_name("HW1")]
-        assert len(h1) == 1
-        h1 = h1[0]
-        h2 = [p for p in water.particles_by_name("HW2")]
-        assert len(h2) == 1
-        h2 = h2[0]
-        m1 = [p for p in water.particles_by_name("MW")]
-        assert len(m1) == 1
-        m1 = m1[0]
-        v1 = h1.xyz[0] - o1.xyz[0]
-        v2 = h2.xyz[0] - o1.xyz[0]
-        v3 = m1.xyz[0] - o1.xyz[0]
-        assert np.allclose(np.linalg.norm(v1), 0.09572)
-        assert np.allclose(np.linalg.norm(v2), 0.09572)
-        assert np.allclose(np.linalg.norm(v3), 0.01577)
-        assert np.allclose(
-            np.degrees(
-                np.arccos(
-                    np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-                )
-            ),
-            104.52,
-        )
-
-    def test_tip4p2005(self):
-        water = WaterTIP4P2005()
-        o1 = [p for p in water.particles_by_name("OW")]
-        assert len(o1) == 1
-        o1 = o1[0]
-        h1 = [p for p in water.particles_by_name("HW1")]
-        assert len(h1) == 1
-        h1 = h1[0]
-        h2 = [p for p in water.particles_by_name("HW2")]
-        assert len(h2) == 1
-        h2 = h2[0]
-        m1 = [p for p in water.particles_by_name("MW")]
-        assert len(m1) == 1
-        m1 = m1[0]
-        v1 = h1.xyz[0] - o1.xyz[0]
-        v2 = h2.xyz[0] - o1.xyz[0]
-        v3 = m1.xyz[0] - o1.xyz[0]
-        assert np.allclose(np.linalg.norm(v1), 0.09572)
-        assert np.allclose(np.linalg.norm(v2), 0.09572)
-        assert np.allclose(np.linalg.norm(v3), 0.01546)
-        assert np.allclose(
-            np.degrees(
-                np.arccos(
-                    np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-                )
-            ),
-            104.52,
+            angle,
         )
