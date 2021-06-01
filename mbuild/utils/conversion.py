7f127d4e544782c4d1a39b7de40362c12a1e2e3c
bc118 <crawfordb118@gmail.com>
2021-06-01 14:15:28 -0400

    fixed test cases rev 3

--- mbuild/utils/conversion.py
+++ mbuild/utils/conversion.py
@@ -22,13 +22,13 @@ def RB_to_OPLS(c0, c1, c2, c3, c4, c5):
     (c0 + c1 + c2 + c3 + c4 + c5) must equal zero, or this conversion is not possible.
     """
     if np.isclose(c5, 0, 1e-12) is False:
-        return ValueError(
+        raise ValueError(
             "ERROR: c5 must equal zero, so this conversion is not possible."
         )
 
     f0 = 2.0 * (c0 + c1 + c2 + c3 + c4 + c5)
     if np.isclose(f0, 0, 1e-12) is False:
-        return ValueError(
+        raise ValueError(
             "ERROR: f0 = 2 * (c0 + c1 + c2 + c3 + c4 + c5) must equal zero, "
             "so this conversion is not possible."
         )
