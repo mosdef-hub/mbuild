fa1e422d9e61febed1b563b530fc9f5e83df1fe0
bc118 <crawfordb118@gmail.com>
2021-08-20 10:27:47 -0400

    added space between RestartCheckpoint and ExpertMode in the gomc_conf_writer.py

--- mbuild/formats/gomc_conf_writer.py
+++ mbuild/formats/gomc_conf_writer.py
@@ -4894,6 +4894,7 @@ class GOMCControl:
         data_control_file.write(
             "{:25s} {}\n".format("RestartCheckpoint", self.RestartCheckpoint)
         )
+        data_control_file.write("\n")
         data_control_file.write(
             "{:25s} {}\n".format("ExpertMode", self.ExpertMode)
         )
