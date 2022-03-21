Fix charge scaling in HOOMD converters (#1011)
* Make correction in charge scaling units

* Update unit

oops--parmed structure is in A not nm

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Update value in tests

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Co Quach <daico007@gmail.com>

mbuild/formats/gsdwriter.py
@@ -132,9 +132,9 @@ def _write_particle_information(
     gsd_snapshot.particles.mass = masses / ref_mass
 
     charges = np.array([atom.charge for atom in structure.atoms])
-    e0 = 2.39725e-4
+    e0 = 2.396452e-04
     """
-    Permittivity of free space = 2.39725e-4 e^2/((kcal/mol)(angstrom)),
+    Permittivity of free space = 2.396452e-04 e^2/((kcal/mol) Angstrom),
     where e is the elementary charge
     """
     charge_factor = (4.0 * np.pi * e0 * ref_distance * ref_energy) ** 0.5

mbuild/formats/hoomdxml.py
@@ -210,7 +210,7 @@ def _write_particle_information(
 
     charges = [atom.charge for atom in structure.atoms]
     xml_file.write("<charge>\n")
-    e0 = 5.72956500956023e-4  # e^2-mol/kJ-nm, permittivity of free space
+    e0 = 2.396452e-04  # e^2 mol/(kcal A), permittivity of free space
     charge_factor = (4.0 * np.pi * e0 * ref_distance * ref_energy) ** 0.5
     for charge in charges:
         xml_file.write("{}\n".format(charge / charge_factor))