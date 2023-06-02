bf5c5fef5f736d04fb8c95f1693b5e90658a296d
Co Quach <daico007@gmail.com>
2023-06-02 01:27:26 -0500

    generalize creation of betacristabolite

--- mbuild/lib/surfaces/betacristobalite.py
+++ mbuild/lib/surfaces/betacristobalite.py
@@ -1,4 +1,9 @@
 """Beta-cristobalite surface."""
+import itertools
+import math
+
+import numpy as np
+
 import mbuild as mb
 
 
@@ -19,18 +24,57 @@ class Betacristobalite(mb.Compound):
     modify the file or the method of determining port locations.
     """
 
-    def __init__(self):
-        super(Betacristobalite, self).__init__()
+    def __init__(self, dimensions=None):
+        super(Betacristobalite, self).__init__(dimensions)
 
-        mb.load(
+        betacristabolite = mb.load(
             "beta-cristobalite-expanded.mol2",
-            compound=self,
             relative_to_module=self.__module__,
             backend="gmso",
         )
         self.periodicity = (True, True, False)
         # 1.3200 taken from boundingbox length rounded to 4 decimal places
-        self.box = mb.Box([5.3888, 4.6669, 1.3200])
+        ref_dims = [5.3888, 4.6669, 1.3200]
+        if not dimensions:
+            box = mb.Box(ref_dims)
+        elif isinstance(dimensions, (list, tuple)) and len(dimensions) == 2:
+            box = mb.Box([dimensions[0], dimensions[1], ref_dims[2]])
+        elif isinstance(dimensions, (list, tuple)) and len(dimensions) == 3:
+            box = mb.Box(dimensions)
+        else:
+            raise ValueError(
+                "Unsupported value for dimension provided. "
+                "Only accept `None` (default), list/tuple of 2 (x and y dimension) "
+                "or list/tuple of 3(x, y, and z dimensions)"
+            )
+
+        self.box = box
+
+        # Borrowed code from water_box recipe
+        scale_Lx = math.ceil(box.Lx / ref_dims[0])
+        scale_Ly = math.ceil(box.Ly / ref_dims[1])
+        scale_Lz = math.ceil(box.Lz / ref_dims[2])
+
+        silica_list = list()
+        for particle in list(betacristabolite.particles()):
+            for i, j, k in itertools.product(
+                range(scale_Lx),
+                range(scale_Ly),
+                range(scale_Lz),
+            ):
+                shift = np.array(
+                    [
+                        i * ref_dims[0],
+                        j * ref_dims[1],
+                        k * ref_dims[2],
+                    ]
+                )
+                if all(particle.pos + shift < box.lengths):
+                    temp = mb.clone(particle)
+                    temp.translate(shift)
+                    silica_list.append(temp)
+
+        self.add(silica_list)
 
         count = 0
         for particle in list(self.particles()):
