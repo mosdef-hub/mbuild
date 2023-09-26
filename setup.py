9d7367ff1b7a2dad5809a9c37a36874869a3b9f4
Co Quach <daico007@gmail.com>
2023-09-26 12:17:51 -0500

    Bump to version 0.16.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 
 #####################################
 NAME = "mbuild"
-VERSION = "0.16.3"
+VERSION = "0.16.4"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
