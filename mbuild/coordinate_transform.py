8649337103e0f340111658c66a2e8c0ed7219840
jac16 <jennifer.clark@nist.gov>
2024-08-19 09:51:36 -0400

    Update defaults

--- mbuild/coordinate_transform.py
+++ mbuild/coordinate_transform.py
@@ -35,7 +35,7 @@ def force_overlap(
     add_bond : bool, optional, default=True
         If `from_positions` and `to_positions` are `Ports`, create a bond
         between the two anchor atoms.
-    reset_labels : bool, optional, default=True
+    reset_labels : bool, optional, default=False
         If True, the Compound labels will be reset, renumbered using the Compound.reset_labels methods
     """
     from mbuild.port import Port
