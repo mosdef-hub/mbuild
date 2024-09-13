c79a58e54f16b76bb540553dd2930da0e758951e
CalCraven <nicholas.c.craven@vanderbilt.edu>
2024-09-12 19:55:49 -0500

    Bump to version 1.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 
 #####################################
 NAME = "mbuild"
-VERSION = "0.18.0"
+VERSION = "1.0.0"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
