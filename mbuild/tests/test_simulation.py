4c16025e868135c6452cc906d9a52b2122fb1689
CalCraven <nicholas.c.craven@vanderbilt.edu>
2026-09-03 10:15:53 -0500

    Generalize fene test for path ff

--- mbuild/tests/test_simulation.py
+++ mbuild/tests/test_simulation.py
@@ -440,6 +440,10 @@ class TestHoomdSimulation(BaseTest):
         pff = PathForcefield(radius=0.4, bond_length={"A-B": 0.25})
         sim = HoomdSimulation(path, forcefield=pff, r_cut=0.5, run_on_gpu=False)
         fene = sim.forces[1]
+        for force in sim.forces:
+            if isinstance(force, hoomd.md.bond.FENEWCA):
+                fene = force
+                break
         assert fene.params["A-B"]["sigma"] == pytest.approx(0.25)
 
     @pytest.mark.skipif(not has_hoomd, reason="hoomd is not installed")
